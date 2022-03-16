474a763148ae8f4f2e30a65a58292accc173381e
amateima <amatei@umaproject.org>
2022-03-16 20:39:22 +0200

    Add lp fee calculator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('development');
-  }
-  return a + b;
-};
+import * as lpFeeCalculator from "./lpFeeCalculator";
+
+export { lpFeeCalculator };
