8671e5d1e0b479ef52dc14206e945f114cc63674
David A <david@umaproject.org>
2022-03-24 07:59:12 -0400

    chore: add pool client with only modifications for building (#5)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,11 @@ module.exports = {
   "extends": [
     "react-app",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
+  rules: {
+    "prettier/prettier": ["warn"],
+  },
   "settings": {
     "react": {
       "version": "999.999.999"
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,12 @@
 import { BigNumber, ethers } from "ethers";
+import * as uma from "@uma/sdk";
+import Decimal from "decimal.js";
 
 export type BigNumberish = string | number | BigNumber;
 export type BN = BigNumber;
+export type Decimalish = string | number | Decimal;
+
+const { ConvertDecimals } = uma.utils;
 
 /**
  * toBN.
@@ -56,3 +61,131 @@ export function max(a: BigNumberish, b: BigNumberish): BN {
 }
 
 export const fixedPointAdjustment = toBNWei("1");
+
+/**
+ * Convert an amount of eth into a token given price and token decimals.
+ *
+ * @param {BigNumberish} fromAmount - Amount of eth to convert.
+ * @param {string | number} [ price=1 ] - The price as eth per token, ie how much eth can 1 token buy.
+ * @param {} [ toDecimals=18 ] - Number of decimals for the token currency.
+ * @returns {string} The number of tokens denominated in token decimals in the smallest unit (wei).
+ */
+export function ethToToken(fromAmount: BigNumberish, price: string | number = 1, toDecimals = 18): string {
+  const priceWei = toBNWei(price);
+  const toAmount = toBNWei(fromAmount).div(priceWei);
+  return ConvertDecimals(18, toDecimals)(toAmount);
+}
+
+/**
+ * Convert a gas amount and gas price to wei.
+ *
+ * @param {number} gas - gas amount.
+ * @param {BigNumberish} gasPrice - gas price in gwei.
+ * @returns {BigNumber} - total fees in wei.
+ */
+export const gasToEth = (gas: number, gasPrice: BigNumberish): BigNumber => {
+  return BigNumber.from(gas).mul(gasPrice);
+};
+
+/**
+ * getGasFees. Low level pure function call to calculate gas fees.
+ *
+ * @param {number} gas - The gast cost for transfer, use constants defined in file.
+ * @param {BigNumberish} gasPrice - Estimated gas price in wei.
+ * @param {string | number} [price = 1] - The price of the token in eth, how much eth can 1 token buy.
+ * @param {number} [decimals=18] - Number of decimals of token.
+ * @returns {string} - The value of fees native to the token/eth provided, in its smallest unit.
+ */
+export function calculateGasFees(
+  gas: number,
+  gasPrice: BigNumberish,
+  price: string | number = 1,
+  decimals = 18
+): string {
+  const amountEth = gasToEth(gas, gasPrice);
+  return ethToToken(amountEth, price, decimals);
+}
+
+/**
+ * percent.
+ *
+ * @param {BigNumberish} numerator
+ * @param {BigNumberish} denominator
+ * @returns {BN}
+ */
+export function percent(numerator: BigNumberish, denominator: BigNumberish): BN {
+  return fixedPointAdjustment.mul(numerator).div(denominator);
+}
+
+/**
+ * calcContinuousCompoundInterest. From https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php?given_data=find_r&A=2&P=1&n=0&t=1&given_data_last=find_r&action=solve
+ * Returns a yearly interest rate if start/end amount had been continuously compounded over the period elapsed. Multiply result by 100 for a %.
+ *
+ * @param {string} startAmount
+ * @param {string} endAmount
+ * @param {string} periodsElapsed
+ * @param {string} periodsPerYear
+ */
+export const calcContinuousCompoundInterest = (
+  startAmount: Decimalish,
+  endAmount: Decimalish,
+  periodsElapsed: Decimalish,
+  periodsPerYear: Decimalish
+): string => {
+  const years = new Decimal(periodsPerYear).div(periodsElapsed);
+  return new Decimal(endAmount)
+    .div(startAmount)
+    .ln()
+    .div(years)
+    .toString();
+};
+/**
+ * calcPeriodicCompoundInterest. Taken from https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php?given_data=find_r&A=2&P=1&n=365&t=1&given_data_last=find_r&action=solve
+ * This will return a periodically compounded interest rate for 1 year. Multiply result by 100 for a %.
+ *
+ * @param {string} startAmount - Starting amount or price
+ * @param {string} endAmount - Ending amount or price
+ * @param {string} periodsElapsed - How many periods elapsed for the start and end amount.
+ * @param {string} periodsPerYear - How many periods in 1 year.
+ */
+export const calcPeriodicCompoundInterest = (
+  startAmount: Decimalish,
+  endAmount: Decimalish,
+  periodsElapsed: Decimalish,
+  periodsPerYear: Decimalish
+): string => {
+  const n = new Decimal(periodsPerYear);
+  const A = new Decimal(endAmount);
+  const P = new Decimal(startAmount);
+  const t = new Decimal(periodsPerYear).div(periodsElapsed);
+  const one = new Decimal(1);
+  return n
+    .mul(
+      A.div(P)
+        .pow(one.div(n.div(t)))
+        .sub(one)
+    )
+    .toString();
+};
+
+/**
+ * calcApr. Simple apr calculation based on extrapolating the difference for a short period over a year.
+ *
+ * @param {Decimalish} startAmount - Starting amount or price
+ * @param {Decimalish} endAmount - Ending amount or price
+ * @param {Decimalish} periodsElapsed - periods elapsed from start to end
+ * @param {Decimalish} periodsPerYear - periods per year
+ */
+export const calcApr = (
+  startAmount: Decimalish,
+  endAmount: Decimalish,
+  periodsElapsed: Decimalish,
+  periodsPerYear: Decimalish
+): string => {
+  return new Decimal(endAmount)
+    .sub(startAmount)
+    .div(startAmount)
+    .mul(periodsPerYear)
+    .div(periodsElapsed)
+    .toString();
+};
