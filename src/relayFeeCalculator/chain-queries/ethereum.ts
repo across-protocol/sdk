f88457c10c7d604bb0b7c28a8501ce62891f5388
Matt Rice <matthewcrice32@gmail.com>
2022-05-11 14:08:21 +0000

    lint
    Signed-off-by: GitHub <noreply@github.com>

--- src/relayFeeCalculator/chain-queries/ethereum.ts
+++ src/relayFeeCalculator/chain-queries/ethereum.ts
@@ -6,50 +6,49 @@ import axios from "axios";
 
 const { parseUnits } = ethers.utils;
 
-
 // Note: these are the mainnet addresses for these symbols meant to be used for pricing.
-export const SymbolMapping: { [symbol: string]: { address: string, decimals: number } } = {
-    "USDC": {
-        address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-        decimals: 6,
-    },
-    "WETH": {
-        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        decimals: 18,
-    },
-    "ETH": {
-        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        decimals: 18,
-    },
-    "UMA": {
-        address: "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828",
-        decimals: 18,
-    },
-    "WBTC": {
-        address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-        decimals: 18,
-    },
-    "BADGER": {
-        address: "0x3472A5A71965499acd81997a54BBA8D852C6E53d",
-        decimals: 18,
-    },
-    "BOBA": {
-        address: "0x42bBFa2e77757C645eeaAd1655E0911a7553Efbc",
-        decimals: 18,
-    },
-    "DAI": {
-        address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-        decimals: 18,
-    },
-    "MATIC": {
-        address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
-        decimals: 18,
-    },
-    "WMATIC": {
-        address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
-        decimals: 18,
-    }
-}
+export const SymbolMapping: { [symbol: string]: { address: string; decimals: number } } = {
+  USDC: {
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    decimals: 6,
+  },
+  WETH: {
+    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    decimals: 18,
+  },
+  ETH: {
+    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    decimals: 18,
+  },
+  UMA: {
+    address: "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828",
+    decimals: 18,
+  },
+  WBTC: {
+    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    decimals: 18,
+  },
+  BADGER: {
+    address: "0x3472A5A71965499acd81997a54BBA8D852C6E53d",
+    decimals: 18,
+  },
+  BOBA: {
+    address: "0x42bBFa2e77757C645eeaAd1655E0911a7553Efbc",
+    decimals: 18,
+  },
+  DAI: {
+    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    decimals: 18,
+  },
+  MATIC: {
+    address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+    decimals: 18,
+  },
+  WMATIC: {
+    address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+    decimals: 18,
+  },
+};
 
 export const evmRelayAverageGas = 50000;
 
--- src/relayFeeCalculator/chain-queries/optimism.ts
+++ src/relayFeeCalculator/chain-queries/optimism.ts
@@ -3,12 +3,11 @@ import { BigNumberish } from "../../utils";
 import ethers from "ethers";
 import { SymbolMapping } from "./ethereum";
 import { Coingecko } from "../../coingecko/Coingecko";
-import { OptimismSpokePool__factory, OptimismSpokePool } from '@across-protocol/contracts-v2';
+import { OptimismSpokePool__factory, OptimismSpokePool } from "@across-protocol/contracts-v2";
 
 import sdk from "@eth-optimism/sdk";
 
 class OptimismQueryInterface implements QueryInterface {
-
   private spokePool: OptimismSpokePool;
   private provider: sdk.L2Provider<ethers.providers.Provider>;
 
@@ -17,12 +16,26 @@ class OptimismQueryInterface implements QueryInterface {
     // TODO: replace with address getter.
     this.spokePool = OptimismSpokePool__factory.connect("0x59485d57EEcc4058F7831f46eE83a7078276b4AE", provider);
   }
-    
+
   async getGasCosts(tokenSymbol: string): Promise<BigNumberish> {
-      // Create a dummy transaction to estimate. Note: the simulated caller would need to be holding weth and have approved the contract.
-      const tx = await this.spokePool.populateTransaction.fillRelay("0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d", "0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d", "0x4200000000000000000000000000000000000006", "10", "10", "1", "1", "1", "1", "1");
-      const populatedTransaction = await new ethers.VoidSigner("0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d", this.provider).populateTransaction(tx);
-      return (await this.provider.estimateTotalGasCost(populatedTransaction)).toString();
+    // Create a dummy transaction to estimate. Note: the simulated caller would need to be holding weth and have approved the contract.
+    const tx = await this.spokePool.populateTransaction.fillRelay(
+      "0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d",
+      "0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d",
+      "0x4200000000000000000000000000000000000006",
+      "10",
+      "10",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    );
+    const populatedTransaction = await new ethers.VoidSigner(
+      "0x9a8f92a830a5cb89a3816e3d267cb7791c16b04d",
+      this.provider
+    ).populateTransaction(tx);
+    return (await this.provider.estimateTotalGasCost(populatedTransaction)).toString();
   }
 
   async getTokenPrice(tokenSymbol: string): Promise<string | number> {
