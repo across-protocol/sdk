a0ad4fb00e859e338cdb0aa4d20bd5d35622e5db
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-08-09 17:49:25 -0400

    improve: update sdk queries to accurately estimate gas costs (#64)
    * feat: add utility function to calculate total gas cost of an unsigned transaction
    
    * feat: Add utility function to generate an unsigned fillRelay tx
    
    * improve: add cohesive call layer to all gas cost estimates
    
    * fix: update e2e test to resolve error
    
    * fix: make provider access modifiers consistent
    
    * docs: add comment to highlight that a hardcoded value is a dummy address
    
    * improve: remove defaultGas artifact from eth query
    
    * test: add unit test for verifying the gasFeePercent is consistent
    
    * nit: improve variable name to serve the general case
    
    * improve: utilize SpokePool base class instead of BaseContract
    
    * nit: highlight that the isL2Provider is effectively optimism-specific
    
    * improve: use specified query address
    
    * docs: improve comment to better highlight the meaning of isL2Provider
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    
    * improve: utilize generic spoke pool factory

--- src/relayFeeCalculator/chain-queries/boba.ts
+++ src/relayFeeCalculator/chain-queries/boba.ts
@@ -1,45 +1,38 @@
 import { QueryInterface } from "../relayFeeCalculator";
-import { BigNumberish } from "../../utils";
+import {
+  BigNumberish,
+  createUnsignedFillRelayTransaction,
+  estimateTotalGasRequiredByUnsignedTransaction,
+} from "../../utils";
 import { utils, providers } from "ethers";
 import { SymbolMapping } from "./ethereum";
 import { Coingecko } from "../../coingecko/Coingecko";
-import { OptimismSpokePool__factory, OptimismSpokePool } from "@across-protocol/contracts-v2";
+import { SpokePool__factory, SpokePool } from "@across-protocol/contracts-v2";
 
 const { parseUnits } = utils;
 
 export class BobaQueries implements QueryInterface {
-  private spokePool: OptimismSpokePool;
+  private spokePool: SpokePool;
 
   constructor(
-    provider: providers.Provider,
+    readonly provider: providers.Provider,
     readonly symbolMapping = SymbolMapping,
     spokePoolAddress = "0xBbc6009fEfFc27ce705322832Cb2068F8C1e0A58",
     private readonly usdcAddress = "0x66a2A913e447d6b4BF33EFbec43aAeF87890FBbc",
     private readonly simulatedRelayerAddress = "0x893d0d70ad97717052e3aa8903d9615804167759"
   ) {
     // TODO: replace with address getter.
-    this.spokePool = OptimismSpokePool__factory.connect(spokePoolAddress, provider);
+    this.spokePool = SpokePool__factory.connect(spokePoolAddress, provider);
   }
 
   async getGasCosts(_tokenSymbol: string): Promise<BigNumberish> {
-    // Create a dummy transaction to estimate. Note: the simulated caller would need to be holding weth and have approved the contract.
-    const gasEstimate = await this.spokePool.estimateGas.fillRelay(
-      "0xBb23Cd0210F878Ea4CcA50e9dC307fb0Ed65Cf6B",
-      "0xBb23Cd0210F878Ea4CcA50e9dC307fb0Ed65Cf6B",
-      this.usdcAddress,
-      "10",
-      "10",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      { from: this.simulatedRelayerAddress }
+    const tx = await createUnsignedFillRelayTransaction(this.spokePool, this.usdcAddress, this.simulatedRelayerAddress);
+    return estimateTotalGasRequiredByUnsignedTransaction(
+      tx,
+      this.simulatedRelayerAddress,
+      this.provider,
+      parseUnits("1", 9)
     );
-
-    // Boba's gas price is hardcoded to 1 gwei.
-    const bobaGasPrice = parseUnits("1", 9);
-    return gasEstimate.mul(bobaGasPrice).toString();
   }
 
   async getTokenPrice(tokenSymbol: string): Promise<number> {
--- src/relayFeeCalculator/chain-queries/polygon.ts
+++ src/relayFeeCalculator/chain-queries/polygon.ts
@@ -1,20 +1,30 @@
 import { QueryInterface } from "../relayFeeCalculator";
-import { BigNumberish } from "../../utils";
-import { providers, BigNumber } from "ethers";
-import { defaultAverageGas, SymbolMapping } from "./ethereum";
+import {
+  BigNumberish,
+  createUnsignedFillRelayTransaction,
+  estimateTotalGasRequiredByUnsignedTransaction,
+} from "../../utils";
+import { providers } from "ethers";
+import { SymbolMapping } from "./ethereum";
 import { Coingecko } from "../../coingecko/Coingecko";
+import { SpokePool__factory, SpokePool } from "@across-protocol/contracts-v2";
 
 export class PolygonQueries implements QueryInterface {
+  private spokePool: SpokePool;
+
   constructor(
     readonly provider: providers.Provider,
-    public readonly averageGas = defaultAverageGas,
-    readonly symbolMapping = SymbolMapping
-  ) {}
+    readonly symbolMapping = SymbolMapping,
+    readonly spokePoolAddress = "0x69B5c72837769eF1e7C164Abc6515DcFf217F920",
+    readonly usdcAddress = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+    readonly simulatedRelayerAddress = "0x893d0D70AD97717052E3AA8903D9615804167759"
+  ) {
+    this.spokePool = SpokePool__factory.connect(spokePoolAddress, provider);
+  }
 
   async getGasCosts(_tokenSymbol: string): Promise<BigNumberish> {
-    return BigNumber.from(await this.provider.getGasPrice())
-      .mul(this.averageGas)
-      .toString();
+    const tx = await createUnsignedFillRelayTransaction(this.spokePool, this.usdcAddress, this.simulatedRelayerAddress);
+    return estimateTotalGasRequiredByUnsignedTransaction(tx, this.simulatedRelayerAddress, this.provider);
   }
 
   async getTokenPrice(tokenSymbol: string): Promise<number> {
