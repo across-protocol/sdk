90a1386049e60d69fde256c1ecbfd89a85a9f714
amateima <amatei@umaproject.org>
2022-08-30 13:24:56 +0300

    Refactor class functions

--- src/merkleDistributor/MerkleDistributor.ts
+++ src/merkleDistributor/MerkleDistributor.ts
@@ -8,7 +8,7 @@ export class MerkleDistributor {
    * @param recipients An object which describes the recipients
    * @param windowIndex Parameter to specify an window index. This allows using the same smart contract for multiple token distributions.
    */
-  static createMerkleDistributionProofs = (recipients: DistributionRecipients, windowIndex: number) => {
+  static createMerkleDistributionProofs(recipients: DistributionRecipients, windowIndex: number) {
     const recipientLeafs = Object.keys(recipients).map((addr: string) =>
       MerkleDistributor.createLeaf(addr, recipients[addr].amount, recipients[addr].accountIndex)
     );
@@ -24,7 +24,7 @@ export class MerkleDistributor {
       };
     }, {} as DistributionRecipientsWithProofs);
     return { recipientsWithProofs, merkleRoot: merkleTree.getHexRoot() };
-  };
+  }
 
   /**
    * Encode the account address, the amount and the account index into a Merkle Tree leaf.
@@ -34,10 +34,10 @@ export class MerkleDistributor {
    * @param accountIndex The account index
    * @returns The Merkle Tree leaf
    */
-  static createLeaf = (account: string, amount: string, accountIndex: number) => {
+  static createLeaf(account: string, amount: string, accountIndex: number) {
     return Buffer.from(
       ethers.utils.solidityKeccak256(["address", "uint256", "uint256"], [account, amount, accountIndex]).slice(2),
       "hex"
     );
-  };
+  }
 }
