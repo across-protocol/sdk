c658ebabcfb70f4c77404bf320334cc933fa240f
Paul <108695806+pxrl@users.noreply.github.com>
2022-10-20 01:04:57 +0200

    fix(gasPriceOracle): Revert to gasPrice for EIP-1559 chains
    The existing use of maxFeePerGas gives a pessimistic view on the
    probable gas cost of the next transaction.

--- src/gasPriceOracle/oracle.ts
+++ src/gasPriceOracle/oracle.ts
@@ -24,17 +24,17 @@ async function legacy(provider: providers.Provider, chainId: number): Promise<Ga
   };
 }
 
-// @todo: Update to ethers 5.7.x to access FeeData.lastBaseFeePerGas
+// @todo: Update to ethers 5.7.x to access FeeData.lastBaseFeePerGas. Use feeData.gasPrice until then.
 // https://github.com/ethers-io/ethers.js/commit/8314236143a300ae81c1dcc27a7a36640df22061
 async function eip1559(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
   const feeData: providers.FeeData = await provider.getFeeData();
 
-  [feeData.maxFeePerGas, feeData.maxPriorityFeePerGas].forEach((field: BigNumber | null) => {
+  [feeData.gasPrice, feeData.maxPriorityFeePerGas].forEach((field: BigNumber | null) => {
     if (!BigNumber.isBigNumber(field) || field.lt(0)) error("getFeeData()", chainId, feeData);
   });
 
   const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas as BigNumber;
-  const maxFeePerGas = feeData.maxFeePerGas as BigNumber;
+  const maxFeePerGas = feeData.gasPrice as BigNumber; // note gasPrice is used.
 
   return {
     maxPriorityFeePerGas: maxPriorityFeePerGas,
