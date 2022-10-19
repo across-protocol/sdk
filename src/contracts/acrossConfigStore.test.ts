ccf2b306d517bd2bc75ad8f34fc6229e20de67b0
James Morris <jaamorris@cs.stonybrook.edu>
2022-10-19 15:27:47 -0400

    fix: :ambulance: relax error on additional parameters
        This change relaxes the requirement that the L1TokenConfigSs conforms exactly to the shape as the expected superstruct object. Now, a mask utility is called which trims off unnecessary parameters before asserting whether the parseL1TokenConfig input is valid.
    
        Breaking Changes: no breaking changes added

---
+++ src/contracts/acrossConfigStore.test.ts
@@ -0,0 +1,23 @@
+import { Client } from "./acrossConfigStore";
+
+describe("Contracts Config Store", () => {
+  const BASE_TRUTH = {
+    rateModel: { UBar: "750000000000000000", R0: "21000000000000000", R1: "0", R2: "600000000000000000" },
+    transferThreshold: "0",
+  };
+  it("should parse parseL1TokenConfig correctly with exactly the right data", () => {
+    const structure = BASE_TRUTH;
+    expect(Client.parseL1TokenConfig(JSON.stringify(structure))).toEqual(BASE_TRUTH);
+  });
+  it("should parse parseL1TokenConfig correctly with additional unneeded params", () => {
+    const structure = {
+      rateModel: { UBar: "750000000000000000", R0: "21000000000000000", R1: "0", R2: "600000000000000000" },
+      transferThreshold: "0",
+      spokeTargetBalances: {
+        "10": { threshold: "50000000000000000000", target: "20000000000000000000" },
+        "42161": { threshold: "100000000000000000000", target: "20000000000000000000" },
+      },
+    };
+    expect(Client.parseL1TokenConfig(JSON.stringify(structure))).toEqual(BASE_TRUTH);
+  });
+});
