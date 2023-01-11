37f8bbb8f038b199008d6c84e69ad864c7e0eded
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-01-11 11:55:17 +0100

    fix: `ChainId` enum naming collision

--- src/transfers-history/adapters/web3/model.ts
+++ src/transfers-history/adapters/web3/model.ts
@@ -1,3 +1,5 @@
+export { CHAIN_IDs } from "../../../constants";
+
 export type Web3Error = {
   error: {
     code: Web3ErrorCode;
@@ -10,17 +12,3 @@ export enum Web3ErrorCode {
 }
 
 export type ChainId = number;
-
-export const CHAIN_IDs = {
-  ARBITRUM_RINKEBY: 421611,
-  RINKEBY: 4,
-  OPTIMISM_KOVAN: 69,
-  KOVAN: 42,
-  MAINNET: 1,
-  OPTIMISM: 10,
-  ARBITRUM: 42161,
-  BOBA: 288,
-  POLYGON: 137,
-  GOERLI: 5,
-  MUMBAI: 80001,
-};
