d28ccd345afadc8f00c63f7db8509603fe68c912
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-04-11 17:34:15 -0400

    feat: add new uba types

--- src/interfaces/UBA.ts
+++ src/interfaces/UBA.ts
@@ -1,5 +1,19 @@
+import { BigNumber } from "ethers";
 import { DepositWithBlock, FillWithBlock, RefundRequestWithBlock } from "./";
 
 export type UbaInflow = DepositWithBlock;
 export type UbaOutflow = FillWithBlock | RefundRequestWithBlock;
 export type UbaFlow = UbaInflow | UbaOutflow;
+
+export type UbaRunningRequest = {
+  type: "refund" | "deposit";
+  amount: BigNumber;
+};
+
+export const isUbaInflow = (flow: UbaFlow): flow is UbaInflow => {
+  return (flow as UbaInflow).depositId !== undefined;
+};
+
+export const isUbaOutflow = (flow: UbaFlow): flow is UbaOutflow => {
+  return !isUbaInflow(flow);
+};
