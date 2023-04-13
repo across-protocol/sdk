fa037c5ef15e238cc607d84adc1da90abc024dfb
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-04-13 15:40:28 +0200

    feat: update deposit typed data utility (#166)

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,7 @@ import { isL2Provider as isOptimismL2Provider, L2Provider } from "@eth-optimism/
 import { SpokePool } from "@across-protocol/contracts-v2";
 import assert from "assert";
 import { GasPriceEstimate, getGasPriceEstimate } from "./gasPriceOracle";
+import { TypedMessage } from "./interfaces/TypedData";
 
 export type BigNumberish = string | number | BigNumber;
 export type BN = BigNumber;
@@ -369,4 +370,61 @@ export async function findBlockAtOrOlder(provider: providers.Provider, desiredLo
     fromBlockTimestamp = fromBlock.timestamp;
   }
   return fromBlockNumber;
+}
+
+export type UpdateDepositDetailsMessageType = {
+  UpdateDepositDetails: [
+    {
+      name: "depositId";
+      type: "uint32";
+    },
+    { name: "originChainId"; type: "uint256" },
+    { name: "updatedRelayerFeePct"; type: "int64" },
+    { name: "updatedRecipient"; type: "address" },
+    { name: "updatedMessage"; type: "bytes" }
+  ];
+};
+
+/**
+ * Utility function to get EIP-712 compliant typed data that can be signed with the JSON-RPC method
+ * `eth_signedTypedDataV4` in MetaMask (https://docs.metamask.io/guide/signing-data.html). The resulting signature
+ * can then be used to call the method `speedUpDeposit` of a `SpokePool.sol` contract.
+ * @param depositId The deposit ID to speed up.
+ * @param originChainId The chain ID of the origin chain.
+ * @param updatedRelayerFeePct The new relayer fee percentage.
+ * @param updatedRecipient The new recipient address.
+ * @param updatedMessage The new message that should be provided to the recipient.
+ * @return EIP-712 compliant typed data.
+ */
+export function getUpdateDepositTypedData(
+  depositId: number,
+  originChainId: number,
+  updatedRelayerFeePct: BigNumber,
+  updatedRecipient: string,
+  updatedMessage: string
+): TypedMessage<UpdateDepositDetailsMessageType> {
+  return {
+    types: {
+      UpdateDepositDetails: [
+        { name: "depositId", type: "uint32" },
+        { name: "originChainId", type: "uint256" },
+        { name: "updatedRelayerFeePct", type: "int64" },
+        { name: "updatedRecipient", type: "address" },
+        { name: "updatedMessage", type: "bytes" },
+      ],
+    },
+    primaryType: "UpdateDepositDetails",
+    domain: {
+      name: "ACROSS-V2",
+      version: "1.0.0",
+      chainId: originChainId,
+    },
+    message: {
+      depositId,
+      originChainId,
+      updatedRelayerFeePct,
+      updatedRecipient,
+      updatedMessage,
+    },
+  };
 }
