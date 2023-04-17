c1e2afdae55d5eb457dc7dfccb17f35ac62fe2fb
nicholaspai <npai.nyc@gmail.com>
2023-04-17 13:17:15 -0400

    WIP

--- src/relayFeeCalculator/chain-queries/ethereum.ts
+++ src/relayFeeCalculator/chain-queries/ethereum.ts
@@ -7,7 +7,7 @@ export class EthereumQueries extends QueryBase {
   constructor(
     provider: providers.Provider,
     symbolMapping = TOKEN_SYMBOLS_MAP,
-    spokePoolAddress = "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381",
+    spokePoolAddress = "0xdd51F496631322954A2aB3FbdbAd676Af4b564B3",
     usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     simulatedRelayerAddress = "0x893d0D70AD97717052E3AA8903D9615804167759",
     coingeckoProApiKey?: string,
