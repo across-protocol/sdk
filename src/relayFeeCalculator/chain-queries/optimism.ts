303d10be0afe0f03df913ccdad939add717e4eaf
nicholaspai <npai.nyc@gmail.com>
2023-04-17 17:56:45 -0400

    WIP

--- src/relayFeeCalculator/chain-queries/optimism.ts
+++ src/relayFeeCalculator/chain-queries/optimism.ts
@@ -8,7 +8,7 @@ export class OptimismQueries extends QueryBase {
   constructor(
     provider: providers.Provider,
     symbolMapping = TOKEN_SYMBOLS_MAP,
-    spokePoolAddress = "0xA5B9d8a0B0Fa04Ba71BDD68069661ED5C0848884",
+    spokePoolAddress = "0x79e5462A3544D122152595cba5eefc617c875190",
     usdcAddress = "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
     simulatedRelayerAddress = "0x893d0d70ad97717052e3aa8903d9615804167759",
     coingeckoProApiKey?: string,
