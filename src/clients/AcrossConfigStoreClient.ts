9e38571e9ebff3cde7fcb4933930590fb822d316
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-05-08 10:02:46 -0400

    refactor: :fire: remove hardcoded catches for various pools

--- src/clients/AcrossConfigStoreClient.ts
+++ src/clients/AcrossConfigStoreClient.ts
@@ -82,21 +82,6 @@ export class AcrossConfigStoreClient {
       throw new Error(`Could not find block for timestamp ${deposit.quoteTimestamp}`);
     }
 
-    // There is one deposit on optimism for DAI that is right before the DAI rate model was added.
-    if (quoteBlock === 14830339) {
-      quoteBlock = 14830390;
-    }
-
-    // Test SNX deposit was before the rate model update for SNX.
-    if (quoteBlock === 14856066) {
-      quoteBlock = 14856211;
-    }
-
-    // Test deposit on Goerli was before rate model update.
-    if (quoteBlock === 8824889) {
-      quoteBlock = 8825016;
-    }
-
     const rateModel = this.getRateModelForBlockNumber(
       l1Token,
       deposit.originChainId,
