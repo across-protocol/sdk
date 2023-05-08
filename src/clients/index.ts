5fb645d9f3c41aca4d5f2a67fddd4c0efbfdae9a
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-05-08 15:46:33 -0400

    fix: export spoke pool client (#207)

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,2 +1,3 @@
 export { GLOBAL_CONFIG_STORE_KEYS, AcrossConfigStoreClient } from "./AcrossConfigStoreClient";
 export { HubPoolClient } from "./HubPoolClient";
+export { SpokePoolClient } from "./SpokePoolClient";
