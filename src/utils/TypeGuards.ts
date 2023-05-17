343ba9744fcc90cc60bc85377925467181d5b2c3
Paul <108695806+pxrl@users.noreply.github.com>
2023-05-17 23:04:04 +0200

    refactor(utils): Migrate relayer-v2 typeguards
    Can reasonably foresee a need for these in the sdk-v2 repository, given
    some of the migrations that are occurring. Will subsequently update the
    relayer-v2 repository to import these after bumping the SDK version.

---
+++ src/utils/TypeGuards.ts
@@ -0,0 +1,15 @@
+export function isPromiseFulfilled<T>(
+  promiseSettledResult: PromiseSettledResult<T>
+): promiseSettledResult is PromiseFulfilledResult<T> {
+  return promiseSettledResult.status === "fulfilled";
+}
+
+export function isPromiseRejected<T>(
+  promiseSettledResult: PromiseSettledResult<T>
+): promiseSettledResult is PromiseRejectedResult {
+  return promiseSettledResult.status === "rejected";
+}
+
+export function isDefined<T>(input: T | null | undefined): input is T {
+  return input !== null && input !== undefined;
+}
