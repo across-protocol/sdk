1ee0b8d831eb353dbc87e2e7ca23d35c4e16be4b
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-06-07 11:21:12 -0400

    feat(UBA): :sparkles: add utilization calculation in the uba utils (#226)

--- src/UBAFeeCalculator/UBAFeeUtility.ts
+++ src/UBAFeeCalculator/UBAFeeUtility.ts
@@ -1,6 +1,8 @@
 import { BigNumber } from "ethers";
 import { MAX_SAFE_JS_INT } from "@uma/common/dist/Constants";
 import { toBN } from "../utils";
+import { HUBPOOL_CHAIN_ID } from "../constants";
+import { parseEther } from "ethers/lib/utils";
 
 /**
  * Computes a linear integral over a piecewise function
@@ -188,4 +190,29 @@ export function getDepositBalancingFee(
   }
 
   return totalFee;
+}
+
+/**
+ * Computes the utilization at a given point in time based on the
+ * current balances and equity of the hub and spoke pool targets.
+ * @param decimals The number of decimals for the token
+ * @param hubBalance The current balance of the hub pool for the token
+ * @param hubEquity The current equity of the hub pool for the token
+ * @param ethSpokeBalance The current balance of the ETH spoke pool for the token
+ * @param targetSpoke The current balance of the target spoke pool for the token - this is a list.
+ * @returns The utilization of the hub pool
+ */
+export function calculateUtilization(
+  decimals: number,
+  hubBalance: BigNumber,
+  hubEquity: BigNumber,
+  ethSpokeBalance: BigNumber,
+  spokeTargets: { target: BigNumber; spokeChainId: number }[]
+) {
+  const numerator = hubBalance
+    .add(ethSpokeBalance)
+    .add(spokeTargets.reduce((a, b) => (b.spokeChainId !== HUBPOOL_CHAIN_ID ? a.add(b.target) : a), BigNumber.from(0)));
+  const denominator = hubEquity;
+  const result = numerator.mul(parseEther("1.0")).div(denominator); // We need to multiply by 1e18 to get the correct precision for the result
+  return BigNumber.from(10).pow(decimals).sub(result);
 }
