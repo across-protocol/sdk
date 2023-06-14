e056dfd3c24df37e4bd2267bc93daf9da96b7643
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-06-14 11:25:31 -0400

    fix: type the relayer calculator
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- src/clients/UBAClient/UBAClientWithRefresh.ts
+++ src/clients/UBAClient/UBAClientWithRefresh.ts
@@ -12,7 +12,7 @@ export class UBAClientWithRefresh extends BaseUBAClient {
   /**
    * The RelayFeeCalculator is used to compute the relayer fee for a given amount of tokens.
    */
-  private readonly relayCalculator;
+  private readonly relayCalculator: RelayFeeCalculator;
 
   // @dev chainIdIndices supports indexing members of root bundle proposals submitted to the HubPool.
   //      It must include the complete set of chain IDs ever supported by the HubPool.
