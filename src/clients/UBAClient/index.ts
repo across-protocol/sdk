21ee5f1f3475d3650bda59b08e0125fb9cf7c1a9
Paul <108695806+pxrl@users.noreply.github.com>
2023-06-17 11:13:30 +0200

    chore(UBA): Export UBA fee calculator types
    Necessary for external users now that the UBAClient exposes public
    methods that require this type as an input.
    
    Note: I exported this directly from the UBAClient directory because the
    user of the UBAClient should be able to import everything it needs from
    that directory. But we might want to do a little re-org to make sure
    that the type itself is defined in the right place.

--- src/clients/UBAClient/index.ts
+++ src/clients/UBAClient/index.ts
@@ -1,2 +1,3 @@
+export { UBAActionType } from "../../UBAFeeCalculator/UBAFeeTypes";
 export { UBAClientManual } from "./UBAClientWithManual";
 export { UBAClientWithRefresh as UBAClient } from "./UBAClientWithRefresh";
