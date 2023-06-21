5aee10599f674179acc0422e10e1962c6c2ee98b
Paul <108695806+pxrl@users.noreply.github.com>
2023-06-21 22:57:38 +0200

    chore(UBA): Additional exports (#253)
    Needed for use in relayer-v2.

--- src/clients/UBAClient/index.ts
+++ src/clients/UBAClient/index.ts
@@ -1,3 +1,4 @@
 export { UBAActionType } from "../../UBAFeeCalculator/UBAFeeTypes";
+export { BalancingFeeReturnType as UBABalancingFee, SystemFeeResult as UBASystemFee } from "./UBAClientAbstract";
 export { UBAClientManual } from "./UBAClientWithManual";
 export { UBAClientWithRefresh as UBAClient } from "./UBAClientWithRefresh";
