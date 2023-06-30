bf5a71ed9028ce7d3aa4fe91c2bea44f8b27c61a
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-06-30 09:41:38 -0400

    improve: change naming of file

--- src/clients/UBAClient/index.ts
+++ src/clients/UBAClient/index.ts
@@ -1,4 +1,4 @@
 export { UBAActionType } from "../../UBAFeeCalculator/UBAFeeTypes";
-export { BalancingFeeReturnType as UBABalancingFee, SystemFeeResult as UBASystemFee } from "./UBAClientAbstract";
-export { UBAClientManual } from "./UBAClientWithManual";
+export { BalancingFeeReturnType as UBABalancingFee, SystemFeeResult as UBASystemFee } from "./UBAClientTypes";
 export { UBAClientWithRefresh as UBAClient } from "./UBAClientWithRefresh";
+export { BaseUBAClient as BaseUBAClient } from "./UBAClientBase";
