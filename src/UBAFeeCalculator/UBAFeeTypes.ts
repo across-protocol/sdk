49a13383d6ab3480f7128faeddad619349ca26b5
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-06-30 14:48:13 -0400

    improve: create analog versions of all uba calc

--- src/UBAFeeCalculator/UBAFeeTypes.ts
+++ src/UBAFeeCalculator/UBAFeeTypes.ts
@@ -1,4 +1,15 @@
+import { BigNumber } from "ethers";
+import { TokenRunningBalance } from "../interfaces";
+
 export enum UBAActionType {
   Deposit = "deposit",
   Refund = "refund",
 }
+
+export type TokenRunningBalanceWithNetSend = TokenRunningBalance & {
+  netRunningBalanceAdjustment: BigNumber;
+};
+
+export type UBAFlowFee = {
+  balancingFee: BigNumber;
+};
