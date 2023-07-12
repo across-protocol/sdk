8b5f6e9280008df22c2ab4b2e35fc3b53ead276f
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-07-12 10:20:27 -0400

    hotfix: hardcode failing condition

--- src/clients/AcrossConfigStoreClient/AcrossConfigStoreClient.ts
+++ src/clients/AcrossConfigStoreClient/AcrossConfigStoreClient.ts
@@ -306,7 +306,8 @@ export class AcrossConfigStoreClient extends BaseAbstractClient {
 
         // For now, we'll need to check if this value is undefined. In the
         // future, we should make this a required field.
-        if (parsedValue?.uba !== undefined) {
+        // eslint-disable-next-line no-constant-condition
+        if (parsedValue?.uba !== undefined && false) {
           // Parse and store UBA config
           const ubaConfig = parseUBAConfigFromOnChain(parsedValue.uba);
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
