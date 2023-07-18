051bedf0a918624af40eddebdf619db7b28620cd
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-07-18 17:51:59 -0400

    chore: optimize update in ubaClient (#320)

--- src/clients/UBAClient/UBAClientWithRefresh.ts
+++ src/clients/UBAClient/UBAClientWithRefresh.ts
@@ -3,7 +3,7 @@ import winston from "winston";
 import { HubPoolClient, SpokePoolClient } from "..";
 import { BaseUBAClient } from "./UBAClientBase";
 import { updateUBAClient } from "./UBAClientUtilities";
-import { UBAChainState } from "./UBAClientTypes";
+import { UBAClientState } from "./UBAClientTypes";
 export class UBAClientWithRefresh extends BaseUBAClient {
   // @dev chainIdIndices supports indexing members of root bundle proposals submitted to the HubPool.
   //      It must include the complete set of chain IDs ever supported by the HubPool.
@@ -25,38 +25,26 @@ export class UBAClientWithRefresh extends BaseUBAClient {
    * Updates the clients and UBAFeeCalculators.
    * @param forceRefresh An optional boolean to force a refresh of the clients.
    */
-  public async update(state: { [chainId: number]: UBAChainState }, forceClientRefresh?: boolean): Promise<void> {
-    if (state) {
-      await super.update(state);
-      return;
-    }
-    // Update the clients if the necessary clients have not been updated at least once.
-    // Also update if forceClientRefresh is true.
-    if (forceClientRefresh || !this.isUpdated) {
-      // Update the Across config store
-      await this.hubPoolClient.configStoreClient.update();
-      // Update the HubPool
-      await this.hubPoolClient.update();
-      // Update the SpokePools
-      await Promise.all(Object.values(this.spokePoolClients).map(async (spokePoolClient) => spokePoolClient.update()));
-    }
-    return this.update(
-      await updateUBAClient(
-        this.hubPoolClient,
-        this.spokePoolClients,
-        this.chainIdIndices,
-        this.tokens,
-        forceClientRefresh,
-        this.maxBundleStates
-      )
-    );
+  public async update(state?: UBAClientState, forceClientRefresh?: boolean): Promise<void> {
+    const newState =
+      state && Object.entries(state).length > 0
+        ? state
+        : await updateUBAClient(
+            this.hubPoolClient,
+            this.spokePoolClients,
+            this.chainIdIndices,
+            this.tokens,
+            forceClientRefresh,
+            this.maxBundleStates
+          );
+    await super.update(newState);
   }
   public get isUpdated(): boolean {
     return (
       this.hubPoolClient.configStoreClient.isUpdated &&
       this.hubPoolClient.isUpdated &&
       Object.values(this.spokePoolClients).every((spokePoolClient) => spokePoolClient.isUpdated) &&
-      this.isUpdated
+      this._isUpdated
     );
   }
   public set isUpdated(value: boolean) {
