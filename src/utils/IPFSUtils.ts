52795ccdb2f5a709fb89217f89442ac601182ea3
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-07-26 10:16:25 -0400

    docs: add comments

--- src/utils/IPFSUtils.ts
+++ src/utils/IPFSUtils.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { BigNumber } from "ethers";
 
 /**
- *  Build an IPFS client for interacting with the IPFS API
+ * Build an IPFS client for interacting with the IPFS API
  * @param APIKey The project ID
  * @param secretAPIKey The project secret
  * @returns An IPFS client for interacting with Pinata
@@ -12,6 +12,12 @@ export function buildIPFSClient(APIKey: string, secretAPIKey: string): PinataCli
   return new PinataClient(APIKey, secretAPIKey);
 }
 
+/**
+ * Retrieves a value from an IPFS gateway
+ * @param contentHash The content hash of the value to retrieve
+ * @param publicGatewayURL The URL of the public IPFS gateway to use
+ * @returns The value, or undefined if it could not be retrieved
+ */
 export async function retrieveValueFromIPFS(
   contentHash: string,
   publicGatewayURL: string
@@ -32,6 +38,12 @@ export async function retrieveValueFromIPFS(
   }
 }
 
+/**
+ * Pins a value to the IPFS network
+ * @param content The value to pin
+ * @param client The IPFS client to use
+ * @returns The content hash of the pinned value
+ */
 export async function storeValueInIPFS(content: string, client: PinataClient): Promise<string> {
   const result = await client.pinJSONToIPFS(JSON.parse(content));
   return result.IpfsHash;
@@ -44,9 +56,10 @@ export async function storeValueInIPFS(content: string, client: PinataClient): P
  * @returns The converted value
  */
 export function jsonReplacerWithBigNumbers(_key: string, value: unknown): unknown {
-  if (typeof value === "bigint") {
-    return value.toString();
-  } else if (value instanceof BigNumber) {
+  // We need to check if this is a big number, because the JSON parser
+  // is not aware of BigNumbers and will convert them to the string representation
+  // of the object itself which is not what we want.
+  if (value instanceof BigNumber) {
     return value.toString();
   }
   return value;
@@ -59,6 +72,8 @@ export function jsonReplacerWithBigNumbers(_key: string, value: unknown): unknow
  * @returns The converted value
  */
 export function jsonReviverWithBigNumbers(_key: string, value: unknown): unknown {
+  // We need to check for both strings and big numbers, because the JSON parser
+  // is not aware of BigNumbers.
   if (typeof value === "string" && /^-?\d+$/.test(value)) {
     const bigNumber = BigNumber.from(value);
     if (bigNumber.toString() === value) {
