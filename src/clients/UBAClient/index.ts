57fa49c126af99d42def23589fcadf21dcf21dea
nicholaspai <npai.nyc@gmail.com>
2023-08-02 10:05:33 -0400

    wip
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- src/clients/UBAClient/index.ts
+++ src/clients/UBAClient/index.ts
@@ -3,3 +3,4 @@ export * from "./UBAClientUtilities";
 export * from "../../UBAFeeCalculator/UBAFeeTypes";
 export * from "./UBAClientTypes";
 export { UBAClientWithRefresh as UBAClient } from "./UBAClientWithRefresh";
+export * from "./UBAClientConfig";
