030938c0c18729797ecdc85748ca3c27c3a19f87
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2023-08-03 15:58:11 -0400

    Update src/clients/HubPoolClient.ts

--- src/clients/HubPoolClient.ts
+++ src/clients/HubPoolClient.ts
@@ -348,7 +348,7 @@ export class HubPoolClient extends BaseAbstractClient {
     chain: number,
     chainIdListOverride?: number[]
   ): number | undefined {
-    const chainIdList = chainIdListOverride ?? this.configStoreClient.getEnabledChains();
+    const chainIdList = chainIdListOverride ?? this.configStoreClient.getEnabledChains(latestMainnetBlock);
     let endingBlockNumber: number | undefined;
     // Search proposed root bundles in reverse chronological order.
     for (let i = this.proposedRootBundles.length - 1; i >= 0; i--) {
