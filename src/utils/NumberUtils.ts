bafd1c18fa06685ceae8692a32d562847b1da224
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-08-04 16:11:20 -0400

    improve: add variant of integer

--- src/utils/NumberUtils.ts
+++ src/utils/NumberUtils.ts
@@ -6,3 +6,12 @@
 export function isInteger(value: unknown): value is number {
   return !Number.isNaN(Number(value)) && Number.isInteger(value);
 }
+
+/**
+ * A typeguard for a number with added checks that the number is an integer and is greater to zero.
+ * @param value The value to check.
+ * @returns True if the value is a number, an integer, and greater than zero, false otherwise.
+ */
+export function isPositiveInteger(value: unknown): value is number {
+  return isInteger(value) && value > 0;
+}
