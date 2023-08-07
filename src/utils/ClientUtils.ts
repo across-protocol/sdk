741e4a6cfad363a05b0a5bbd0dcbe83324b37ca9
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-08-07 10:44:23 -0400

    improve: include name of client

--- src/utils/ClientUtils.ts
+++ src/utils/ClientUtils.ts
@@ -6,6 +6,6 @@ import assert from "assert";
  * @param clients The clients to check
  * @throws AssertionError if the clients are not updated
  */
-export function assertClientsAreUpdated(...clients: BaseAbstractClient[]): void {
-  clients.forEach((client) => assert(client.isUpdated, "Client is not updated"));
+export function assertClientsAreUpdated(...clients: (BaseAbstractClient | undefined | null)[]): void {
+  clients.forEach((client) => !!client && assert(client.isUpdated, `${client.clientName ?? "Client"} is not updated`));
 }
