3bf5a5e52075c3df701d4496a69724d72d1e3175
Paul <108695806+pxrl@users.noreply.github.com>
2023-08-07 12:22:27 +0200

    Fix USDC token address

--- src/relayFeeCalculator/chain-queries/zksync.ts
+++ src/relayFeeCalculator/chain-queries/zksync.ts
@@ -8,7 +8,7 @@ export class ZkSyncQueries extends QueryBase {
     provider: providers.Provider,
     symbolMapping = TOKEN_SYMBOLS_MAP,
     spokePoolAddress = "0xE0B015E54d54fc84a6cB9B666099c46adE9335FF",
-    usdcAddress = "0x0faF6df7054946141266420b43783387A78d82A9",
+    usdcAddress = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
     simulatedRelayerAddress = "0x893d0d70ad97717052e3aa8903d9615804167759",
     coingeckoProApiKey?: string,
     logger: Logger = DEFAULT_LOGGER,
