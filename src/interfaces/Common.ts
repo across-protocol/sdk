b365b9f4682e5c2f65a491c920890d9ba0f9735a
nicholaspai <npai.nyc@gmail.com>
2023-08-08 00:35:11 -0400

    add

--- src/interfaces/Common.ts
+++ src/interfaces/Common.ts
@@ -10,3 +10,26 @@ export interface SortableEvent {
 export interface BigNumberForToken {
   [l1TokenAddress: string]: BigNumber;
 }
+
+export interface _Block {
+  hash: string;
+  parentHash: string;
+  number: number;
+
+  timestamp: number;
+  nonce: string;
+  difficulty: number;
+  _difficulty: BigNumber;
+
+  gasLimit: BigNumber;
+  gasUsed: BigNumber;
+
+  miner: string;
+  extraData: string;
+
+  baseFeePerGas?: null | BigNumber;
+}
+
+export interface Block extends _Block {
+  transactions: Array<string>;
+}
