ab435d146c9261fac4dfc329c4971a4342f2d967
nicholaspai <npai.nyc@gmail.com>
2023-08-09 20:04:50 -0400

    WIP

--- src/clients/UBAClient/UBAClientConfig.ts
+++ src/clients/UBAClient/UBAClientConfig.ts
@@ -1,14 +1,12 @@
 /**
  * Defines the configuration to customize a UBAClient instance.
  */
-class UBAClientConfig {
+export class UBAClientConfig {
   /**
    * Instantiate a new UBAClient Config object
    * @param latestMainnetBundleStartBlockToLoadFromCache If defined, the UBAClient will only load bundle data from
    * the cache from bundles whose mainnet start block is greater than or equal to this value. All older bundles
    * will be loaded from the cache and all newer bundles will be loaded fresh from new RPC data.
    */
-  constructor(public readonly latestMainnetBundleStartBlockToLoadFromCache: number | undefined) {}
+  constructor(public readonly latestMainnetBundleStartBlockToLoadFromCache?: number) {}
 }
-
-export default UBAClientConfig;
