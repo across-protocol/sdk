17741268ca2d35d844e330d4b416f353be4b20e2
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2023-08-14 12:45:28 -0400

    feat: Add Public Networks (#373)

--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,22 @@ export const HUBPOOL_CHAIN_ID = 1;
  * before the dynamic config store was introduced.
  */
 export const PROTOCOL_DEFAULT_CHAIN_ID_INDICES = [1, 10, 137, 288, 42161];
+
+export const PUBLIC_NETWORKS: { [chainId: number]: { name: string; etherscan: string } } = {
+  1: {
+    name: "mainnet",
+    etherscan: "https://etherscan.io/",
+  },
+  5: { name: "goerli", etherscan: "https://goerli.etherscan.io/" },
+  10: { name: "optimism", etherscan: "https://optimistic.etherscan.io/" },
+  137: {
+    name: "polygon-matic",
+    etherscan: "https://polygonscan.com/",
+  },
+  324: { name: "zksync", etherscan: "https://explorer.zksync.io/" },
+  8453: { name: "base", etherscan: "https://mainnet.basescan.org" },
+  42161: { name: "arbitrum", etherscan: "https://arbiscan.io/" },
+  43114: { name: "avalanche", etherscan: "https://snowtrace.io/" },
+  84531: { name: "base-goerli", etherscan: "https://basescan.org" },
+  421613: { name: "arbitrum-goerli", etherscan: "https://goerli.arbiscan.io/" },
+};
