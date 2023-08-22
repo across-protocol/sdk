ba2b9ca37881f60d089cf1d179a353d3c6c15223
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-08-22 15:39:19 -0400

    fix: correct etherscan link

--- src/constants.ts
+++ src/constants.ts
@@ -31,6 +31,6 @@ export const PUBLIC_NETWORKS: { [chainId: number]: { name: string; etherscan: st
   8453: { name: "base", etherscan: "https://mainnet.basescan.org" },
   42161: { name: "arbitrum", etherscan: "https://arbiscan.io/" },
   43114: { name: "avalanche", etherscan: "https://snowtrace.io/" },
-  84531: { name: "base-goerli", etherscan: "https://basescan.org" },
+  84531: { name: "base-goerli", etherscan: "https://basescan.org/" },
   421613: { name: "arbitrum-goerli", etherscan: "https://goerli.arbiscan.io/" },
 };
