9241e8a350b9b4bbee3ae013b8b65844ca5feb88
Paul <108695806+pxrl@users.noreply.github.com>
2023-09-22 17:22:09 +0200

    feat: Utility for computing average block times (#406)
    Proposed to replace the current utility from UMA, which hardcodes the
    block times and requires manual updates to support new chains.

---
+++ src/utils/BlockUtils.e2e.ts
@@ -0,0 +1,34 @@
+import { providers } from "ethers";
+import { averageBlockTime } from "./BlockUtils";
+
+type Provider = providers.Provider;
+
+const networks: { [chainId: number]: string } = {
+  1: "https://eth.llamarpc.com",
+  10: "https://optimism.llamarpc.com",
+  137: "https://polygon.llamarpc.com",
+  384: "https://mainnet.era.zksync.io",
+  8453: "https://mainnet.base.org",
+  42161: "https://arbitrum.llamarpc.com",
+};
+
+describe("BlockUtils", () => {
+  let rpcProviders: Provider[] = [];
+
+  beforeAll(() => {
+    rpcProviders = Object.entries(networks).map(([chainId, _rpcUrl]) => {
+      const rpcUrl: string = process.env[`NODE_URL_${chainId}`] ?? _rpcUrl;
+      const provider = new providers.StaticJsonRpcProvider(rpcUrl);
+      return provider;
+    });
+  });
+
+  it("Compute average block times", async () => {
+    for (const provider of rpcProviders) {
+      const chainId = (await provider.getNetwork()).chainId;
+
+      const { average, blockRange } = await averageBlockTime(provider);
+      console.log(`Got average block time over ${blockRange} blocks for chain ${chainId}: ${average}.`);
+    }
+  });
+});
