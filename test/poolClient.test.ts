f9f4934248bd114f70d1c6f514e1b93323272f40
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-09-28 18:28:04 -0400

    feat: hubpool migration (#412)

--- test/poolClient.test.ts
+++ test/poolClient.test.ts
@@ -2,17 +2,21 @@ import * as poolClient from "../src/pool/poolClient";
 import { BigNumber } from "ethers";
 import { expect } from "./utils";
 
-it("previewRemoval", function () {
-  const user = {
-    address: "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
-    lpTokens: "900000000000000000",
-    positionValue: "900000541941830509",
-    totalDeposited: "900000000000000000",
-    feesEarned: "541941830509",
-  };
-  const result = poolClient.previewRemoval(user, 0.75);
+describe("poolClient", function () {
+  it("previewRemoval", function () {
+    const user = {
+      address: "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
+      lpTokens: "900000000000000000",
+      positionValue: "900000541941830509",
+      totalDeposited: "900000000000000000",
+      feesEarned: "541941830509",
+    };
+    const result = poolClient.previewRemoval(user, 0.75);
 
-  expect(BigNumber.from(result.total.recieve).add(result.total.remain).toString()).to.be.eq(user.positionValue);
-  expect(BigNumber.from(result.position.recieve).add(result.position.remain).toString()).to.be.eq(user.totalDeposited);
-  expect(BigNumber.from(result.fees.recieve).add(result.fees.remain).toString()).to.be.eq(user.feesEarned);
+    expect(BigNumber.from(result.total.recieve).add(result.total.remain).toString()).to.be.eq(user.positionValue);
+    expect(BigNumber.from(result.position.recieve).add(result.position.remain).toString()).to.be.eq(
+      user.totalDeposited
+    );
+    expect(BigNumber.from(result.fees.recieve).add(result.fees.remain).toString()).to.be.eq(user.feesEarned);
+  });
 });
