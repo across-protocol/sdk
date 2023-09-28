2d1d1680b38ba638d1f4883e09d97879f53b1a74
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-09-28 13:06:18 -0400

    chore(refactor): move spokepool client tests (#409)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,13 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig } from "hardhat/config";
-import { getNodeUrl, getMnemonic } from "@uma/common";
-
-// Custom tasks to add to HRE.
-// FIXME: Temporarily commenting out tasks to minimize amount of files imported and executed at compile time
-// to construct the Hardhat Runtime Environment. Generally we'd prefer to keep the HRE construction
-// lightweight and put runnable scripts in the `scripts` directory rather than add as an HRE task.
-// TODO: Figure out which imported module in `./tasks` is causing HRE construction to fail.
-// require("./tasks");
-
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "hardhat-deploy";
 import "@openzeppelin/hardhat-upgrades";
-
-dotenv.config();
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/config";
+import "solidity-coverage";
 
 const solcVersion = "0.8.18";
-const mnemonic = getMnemonic();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -29,71 +15,7 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: { accounts: { accountsBalance: "1000000000000000000000000" } },
-    mainnet: {
-      url: getNodeUrl("mainnet", true, 1),
-      accounts: { mnemonic },
-      saveDeployments: true,
-      chainId: 1,
-    },
-    kovan: {
-      url: getNodeUrl("kovan", true, 42),
-      accounts: { mnemonic },
-      saveDeployments: true,
-      chainId: 42,
-    },
-    "optimism-kovan": {
-      url: getNodeUrl("optimism-kovan", true, 69),
-      accounts: { mnemonic },
-      saveDeployments: true,
-      chainId: 69,
-      companionNetworks: { l1: "kovan" },
-    },
-    optimism: {
-      url: getNodeUrl("optimism", true, 10),
-      accounts: { mnemonic },
-      saveDeployments: true,
-      chainId: 10,
-      companionNetworks: { l1: "mainnet" },
-    },
-    arbitrum: {
-      chainId: 42161,
-      url: getNodeUrl("arbitrum", true, 42161),
-      saveDeployments: true,
-      accounts: { mnemonic },
-      companionNetworks: { l1: "mainnet" },
-    },
-    zksync: {
-      chainId: 324,
-      url: "https://mainnet.era.zksync.io",
-      saveDeployments: true,
-      accounts: { mnemonic },
-      companionNetworks: { l1: "mainnet" },
-      zksync: true,
-    },
-    "arbitrum-rinkeby": {
-      chainId: 421611,
-      url: getNodeUrl("arbitrum-rinkeby", true, 421611),
-      saveDeployments: true,
-      accounts: { mnemonic },
-      companionNetworks: { l1: "rinkeby" },
-    },
-    rinkeby: {
-      chainId: 4,
-      url: getNodeUrl("rinkeby", true, 4),
-      saveDeployments: true,
-      accounts: { mnemonic },
-    },
-    base: {
-      chainId: 8453,
-      url: "https://mainnet.base.org",
-      saveDeployments: true,
-      accounts: { mnemonic },
-      companionNetworks: { l1: "mainnet" },
-    },
   },
-  gasReporter: { enabled: process.env.REPORT_GAS !== undefined, currency: "USD" },
-  etherscan: { apiKey: process.env.ETHERSCAN_API_KEY },
-  namedAccounts: { deployer: 0 },
   mocha: {
     timeout: 100000,
   },
