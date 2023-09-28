76a00d484ea18a8358a281cfd9c0b19f28062d50
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-09-28 12:48:46 -0400

    fix: remove uma financial lib dep

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "@across-protocol/contracts-v2/dist/test-utils";
-export * from "@uma/financial-templates-lib";
 
 export * from "./utils";
 export * from "./BlockchainUtils";
--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,7 +1,6 @@
 import * as utils from "@across-protocol/contracts-v2/dist/test-utils";
 
 import { TokenRolesEnum } from "@uma/common";
-import { SpyTransport, bigNumberFormatter } from "@uma/financial-templates-lib";
 import { providers } from "ethers";
 import {
   AcrossConfigStoreClient as ConfigStoreClient,
@@ -28,9 +27,10 @@ import chai, { expect } from "chai";
 import chaiExclude from "chai-exclude";
 import _ from "lodash";
 import sinon from "sinon";
-import winston from "winston";
+import winston, { LogEntry } from "winston";
 import { ContractsV2SlowFill, SpokePoolDeploymentResult, SpyLoggerResult } from "../types";
 import { PROTOCOL_DEFAULT_CHAIN_ID_INDICES } from "../../src/constants";
+import { SpyTransport } from "./SpyTransport";
 
 chai.use(chaiExclude);
 
@@ -686,3 +686,43 @@ export function getLastBlockNumber(): Promise<number> {
 export function convertMockedConfigClient(client: unknown): client is ConfigStoreClient {
   return true;
 }
+
+// Iterate over each element in the log and see if it is a big number. if it is, then try casting it to a string to
+// make it more readable. If something goes wrong in parsing the object (it's too large or something else) then simply
+// return the original log entry without modifying it.
+export function bigNumberFormatter(logEntry: LogEntry) {
+  type SymbolRecord = Record<string | symbol, unknown>;
+  try {
+    // Out is the original object if and only if one or more BigNumbers were replaced.
+    const out = iterativelyReplaceBigNumbers(logEntry);
+
+    // Because winston depends on some non-enumerable symbol properties, we explicitly copy those over, as they are not
+    // handled in iterativelyReplaceBigNumbers. This only needs to happen if logEntry is being replaced.
+    if (out !== logEntry)
+      Object.getOwnPropertySymbols(logEntry).map(
+        (symbol) => (out[symbol] = (logEntry as unknown as SymbolRecord)[symbol])
+      );
+    return out as LogEntry;
+  } catch (_) {
+    return logEntry;
+  }
+}
+
+// Traverse a potentially nested object and replace any element that is either a Ethers BigNumber or web3 BigNumber
+// with the string version of it for easy logging.
+const iterativelyReplaceBigNumbers = (obj: Record<string | symbol, unknown> | object) => {
+  // This does a DFS, recursively calling this function to find the desired value for each key.
+  // It doesn't modify the original object. Instead, it creates an array of keys and updated values.
+  const replacements = Object.entries(obj).map(([key, value]): [string, unknown] => {
+    if (BigNumber.isBigNumber(value)) return [key, value.toString()];
+    else if (typeof value === "object" && value !== null) return [key, iterativelyReplaceBigNumbers(value)];
+    else return [key, value];
+  });
+
+  // This will catch any values that were changed by value _or_ by reference.
+  // If no changes were detected, no copy is needed and it is fine to discard the copy and return the original object.
+  const copyNeeded = replacements.some(([key, value]) => obj[key] !== value);
+
+  // Only copy if something changed. Otherwise, return the original object.
+  return copyNeeded ? Object.fromEntries(replacements) : obj;
+};
