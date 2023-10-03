27ba47620f263b943eae65e9d8164b7bf5843814
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-10-03 18:03:48 -0400

    refactor: include caching utils (#422)
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>

--- test/utils/SpyTransport.ts
+++ test/utils/SpyTransport.ts
@@ -20,3 +20,25 @@ export class SpyTransport extends Transport {
     callback();
   }
 }
+
+export function spyLogIncludes(spy: Spy, messageIndex: number, value: string): boolean {
+  // Sinon's getCall(n) function returns values sent in the nth (zero-indexed) call to the spy. Flatten the whole object
+  // and any log messages included and check if the provided value is within the object.
+  // Some calls embed a LOT of data within the errors, such as hardhat contract reverts which include the full solidity
+  // source code. In this case the JSON.strigify will fail on the full message object. To accommodate this we can simplify
+  // what we search for in the log message to simply the search space.
+  try {
+    const lastLogMessage = JSON.stringify([
+      spy.getCall(messageIndex).lastArg,
+      spy.getCall(messageIndex).lastArg.error ? spy.getCall(messageIndex).lastArg.error.message : "", // If there is an error, add its message.
+    ]);
+    return lastLogMessage.indexOf(value) !== -1;
+  } catch {
+    const lastLogMessage = JSON.stringify(spy.getCall(messageIndex).lastArg.message);
+    return lastLogMessage.indexOf(value) !== -1;
+  }
+}
+
+export function lastSpyLogIncludes(spy: Spy, value: string): boolean {
+  return spyLogIncludes(spy, -1, value);
+}
