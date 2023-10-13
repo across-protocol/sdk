fe9d4251b9ca527bcbf384f1525ed895a8d72730
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-10-13 14:03:24 -0400

    improve: add block values

--- src/utils/ValidatorUtils.ts
+++ src/utils/ValidatorUtils.ts
@@ -1,20 +1,21 @@
 import { BigNumber, ethers } from "ethers";
-import { object, number as Number, min as Min, define, optional, string } from "superstruct";
-import { Deposit } from "../interfaces";
+import { object, min as Min, define, optional, string, integer } from "superstruct";
+import { DepositWithBlock } from "../interfaces";
 
 const AddressValidator = define<string>("AddressValidator", (v) => ethers.utils.isAddress(String(v)));
+const HexValidator = define<string>("HexValidator", (v) => ethers.utils.isHexString(String(v)));
 const BigNumberValidator = define<BigNumber>("BigNumberValidator", (v) => ethers.BigNumber.isBigNumber(v));
 
 const DepositSchema = object({
-  depositId: Min(Number(), 0),
+  depositId: Min(integer(), 0),
   depositor: AddressValidator,
   recipient: AddressValidator,
   originToken: AddressValidator,
   amount: BigNumberValidator,
-  originChainId: Min(Number(), 0),
-  destinationChainId: Min(Number(), 0),
+  originChainId: Min(integer(), 0),
+  destinationChainId: Min(integer(), 0),
   relayerFeePct: BigNumberValidator,
-  quoteTimestamp: Min(Number(), 0),
+  quoteTimestamp: Min(integer(), 0),
   realizedLpFeePct: optional(BigNumberValidator),
   destinationToken: AddressValidator,
   message: string(),
@@ -22,8 +23,14 @@ const DepositSchema = object({
   newRelayerFeePct: optional(BigNumberValidator),
   updatedRecipient: optional(string()),
   updatedMessage: optional(string()),
+  blockNumber: Min(integer(), 0),
+  transactionIndex: Min(integer(), 0),
+  logIndex: Min(integer(), 0),
+  quoteBlockNumber: Min(integer(), 0),
+  transactionHash: HexValidator,
+  blockTimestamp: optional(Min(integer(), 0)),
 });
 
-export function isDepositFormedCorrectly(deposit: unknown): deposit is Deposit {
+export function isDepositFormedCorrectly(deposit: unknown): deposit is DepositWithBlock {
   return DepositSchema.is(deposit);
 }
--- test/validatorUtils.test.ts
+++ test/validatorUtils.test.ts
@@ -3,10 +3,12 @@ import { utils, interfaces } from "../src";
 import { ZERO_ADDRESS } from "../src/constants";
 import { toBN } from "@across-protocol/contracts-v2";
 import { cloneDeep } from "lodash";
+import { objectWithBigNumberReviver } from "../src/utils";
+import { Deposit } from "../src/interfaces";
 
 describe("validatorUtils", () => {
   describe("isDeposit", () => {
-    let deposit: interfaces.Deposit;
+    let deposit: interfaces.DepositWithBlock;
     beforeEach(() => {
       deposit = {
         depositId: 1,
@@ -21,6 +23,12 @@ describe("validatorUtils", () => {
         originToken: ZERO_ADDRESS,
         relayerFeePct: toBN(0),
         destinationToken: ZERO_ADDRESS,
+        transactionHash: "0xa",
+        blockNumber: 0,
+        transactionIndex: 0,
+        logIndex: 0,
+        quoteBlockNumber: 0,
+        blockTimestamp: 0,
       };
     });
 
@@ -75,6 +83,13 @@ describe("validatorUtils", () => {
         // Ensure that the deposit is now invalid
         expect(utils.isDepositFormedCorrectly(depositCopy)).to.be.false;
       }
+    });
+    it("should successfully rehydrate real deposits", () => {
+      const deposits: string[] = [
+        '{"amount":{"type":"BigNumber","hex":"0x038d7ea4c68000"},"originChainId":42161,"destinationChainId":10,"relayerFeePct":{"type":"BigNumber","hex":"0xee042a4c72e9a8"},"depositId":1160366,"quoteTimestamp":1697088000,"originToken":"0x82aF49447D8a07e3bd95BD0d56f35241523fBab1","recipient":"0x525D59654479cFaED622C1Ca06f237ce1072c2AB","depositor":"0x269727F088F16E1Aea52Cf5a97B1CD41DAA3f02D","message":"0x","blockNumber":139874261,"transactionIndex":1,"logIndex":1,"transactionHash":"0x4c4273f4cceb288a76aa7d6c057a8e3ab571a19711a59a965726e06b04e6b821","realizedLpFeePct":{"type":"BigNumber","hex":"0x016b90ac8ef5b9"},"destinationToken":"0x4200000000000000000000000000000000000006","quoteBlockNumber":18332204}',
+      ];
+      const rehydratedDeposits = deposits.map((d) => JSON.parse(d, objectWithBigNumberReviver) as Deposit);
+      expect(rehydratedDeposits.every(utils.isDepositFormedCorrectly)).to.be.true;
     });
   });
 });
