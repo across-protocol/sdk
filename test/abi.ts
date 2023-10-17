dc147bd8cbb2ed0a99587e37e614376b0ed25053
Paul <108695806+pxrl@users.noreply.github.com>
2023-10-17 22:38:24 +0200

    feat: ABI retrieval utility (#420)
    Intended to replace UMA's getABI() implementation.

---
+++ test/abi.ts
@@ -0,0 +1,22 @@
+import * as fs from "fs/promises";
+import { getABI, getABIDir } from "../src/utils/abi";
+import { assertPromiseError, assertPromisePasses } from "./utils";
+
+describe("ABI Utils", () => {
+  describe("getABI", () => {
+    it("All files are valid JSON", async () => {
+      const abiFiles = await fs.readdir(getABIDir(), { encoding: "utf8" });
+
+      // Strip any trailing '.json', since readdir() returns the
+      // full filename but callers should only supply the ABI name.
+      for (const abiFile of abiFiles.map((abi) => abi.slice(0, abi.lastIndexOf(".json")))) {
+        await assertPromisePasses(getABI(abiFile));
+      }
+    });
+
+    it("Correctly handles missing contracts", async () => {
+      const contractName = "missing-contract";
+      await assertPromiseError(getABI(contractName), `Unable to retrieve ${contractName} ABI`);
+    });
+  });
+});
