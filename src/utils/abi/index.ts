d976a5471c5808a25d369977a6bf5cd63ced1644
Paul <108695806+pxrl@users.noreply.github.com>
2023-10-17 15:37:15 +0200

    improve test

--- src/utils/abi/index.ts
+++ src/utils/abi/index.ts
@@ -10,13 +10,21 @@ import { isError } from "../../typeguards";
  */
 
 /**
+ * @notice Obtain the path to the local ABI JSON store.
+ * @returns Fully-qualified path to the local ABI JSON store.
+ */
+export function getABIDir(): string {
+  return `${__dirname}/contracts`;
+}
+
+/**
  * @notice Retrieve an ABI desription from the set of known contracts.
  * @param contractName Name of the contract ABI to retrieve.
  * @returns Contract ABI as an ethers ContractInterface type.
  */
 export async function getABI(contractName: string): Promise<ContractInterface> {
   try {
-    return JSON.parse(await fs.readFile(`${__dirname}/contracts/${contractName}.json`, { encoding: "utf8" }));
+    return JSON.parse(await fs.readFile(`${getABIDir()}/${contractName}.json`, { encoding: "utf8" }));
   } catch (err) {
     // @dev fs methods can return errors that are not Error objects (i.e. errno).
     const msg = isError(err) ? err.message : (err as Record<string, unknown>)?.code;
--- test/abi.ts
+++ test/abi.ts
@@ -1,18 +1,22 @@
-import { getABI } from "../src/utils/abi";
+import * as fs from "fs/promises";
+import { getABI, getABIDir } from "../src/utils/abi";
 import { assertPromiseError, expect } from "./utils";
 
 describe("ABI Utils", () => {
-  describe("getABI", async () => {
-    it("Retrieve Multicall3 ABI", async () => {
-      expect(await getABI("Multicall3"));
+  describe("getABI", () => {
+    it("All files are valid JSON", async () => {
+      const abiFiles = await fs.readdir(getABIDir(), { encoding: "utf8" });
+
+      // Strip any trailing '.json', since readdir() returns the
+      // full filename but callers should only supply the ABI name.
+      for (const abiFile of abiFiles.map((abi) => abi.slice(0, abi.lastIndexOf(".json")))) {
+        expect(await getABI(abiFile)).to.not.throw;
+      }
     });
 
     it("Correctly handles missing contracts", async () => {
       const contractName = "missing-contract";
-      await assertPromiseError(
-        getABI(contractName),
-        `Unable to retrieve ${contractName} ABI`
-      );
+      await assertPromiseError(getABI(contractName), `Unable to retrieve ${contractName} ABI`);
     });
   });
 });
