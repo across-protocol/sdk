4b9eca9dcbe3b11a0828809165ec1f8a7bde0c60
Paul <108695806+pxrl@users.noreply.github.com>
2023-11-01 16:31:02 +0100

    fix: lint (#451)

--- src/relayFeeCalculator/chain-queries/ethereum.ts
+++ src/relayFeeCalculator/chain-queries/ethereum.ts
@@ -3,7 +3,7 @@ import {
   CHAIN_IDs,
   DEFAULT_SIMULATED_RELAYER_ADDRESS,
   DEFAULT_SIMULATED_RELAYER_ADDRESS_TEST,
-  TOKEN_SYMBOLS_MAP
+  TOKEN_SYMBOLS_MAP,
 } from "../../constants";
 import { getDeployedAddress } from "../../utils/DeploymentUtils";
 import { DEFAULT_LOGGER, Logger } from "../relayFeeCalculator";
