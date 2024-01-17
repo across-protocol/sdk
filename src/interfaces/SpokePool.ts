c71a21e756bbaac97323e1903744efce99b0b47c
Paul <108695806+pxrl@users.noreply.github.com>
2024-01-17 05:23:38 +0100

    Typo

--- src/interfaces/SpokePool.ts
+++ src/interfaces/SpokePool.ts
@@ -170,7 +170,7 @@ export type v2FillWithBlockStringified = Omit<
 
 export type v3FillWithBlockStringified = Omit<
   v3FillWithBlock,
-  "inoutAmount" | "outputAmount" | "updatableRelayData"
+  "inputAmount" | "outputAmount" | "updatableRelayData"
 > & {
   inputAmount: string;
   outputAmount: string;
