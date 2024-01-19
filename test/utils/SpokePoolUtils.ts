427272520d805850e45255d9f537f52b77ccf8ae
Paul <108695806+pxrl@users.noreply.github.com>
2024-01-19 15:40:43 +0100

    Add SpeedUps

--- test/utils/SpokePoolUtils.ts
+++ test/utils/SpokePoolUtils.ts
@@ -1,7 +1,23 @@
-import { DepositWithBlock, Fill, FillWithBlock, RefundRequest } from "../../src/interfaces";
-import { toBN } from "../../src/utils";
+import {
+  DepositWithBlock,
+  Fill,
+  FillType,
+  FillWithBlock,
+  RefundRequest,
+  v2DepositWithBlock,
+  v2Fill,
+  v3DepositWithBlock,
+  v3Fill,
+} from "../../src/interfaces";
+import { bnZero, isV2Deposit, isV2Fill, toBN } from "../../src/utils";
 
 export function fillFromDeposit(deposit: DepositWithBlock, relayer: string): Fill {
+  return isV2Deposit(deposit)
+    ? v2FillFromDeposit(deposit, relayer)
+    : v3FillFromDeposit(deposit, relayer);
+}
+
+export function v2FillFromDeposit(deposit: v2DepositWithBlock, relayer: string): v2Fill {
   const { recipient, message, relayerFeePct } = deposit;
 
   const fill: Fill = {
@@ -12,7 +28,7 @@ export function fillFromDeposit(deposit: DepositWithBlock, relayer: string): Fil
     depositor: deposit.depositor,
     destinationToken: deposit.destinationToken,
     relayerFeePct: deposit.relayerFeePct,
-    realizedLpFeePct: deposit.realizedLpFeePct ?? toBN(0),
+    realizedLpFeePct: deposit.realizedLpFeePct ?? bnZero,
     recipient,
     relayer,
     message,
@@ -27,16 +43,41 @@ export function fillFromDeposit(deposit: DepositWithBlock, relayer: string): Fil
       message: deposit.updatedMessage ?? message,
       relayerFeePct: deposit.newRelayerFeePct ?? relayerFeePct,
       isSlowRelay: false,
-      payoutAdjustmentPct: toBN(0),
+      payoutAdjustmentPct: bnZero,
     },
   };
 
   return fill;
 }
 
+export function v3FillFromDeposit(deposit: v3DepositWithBlock, relayer: string): v3Fill {
+  const { blockNumber, transactionHash, transactionIndex, ...partialDeposit } = deposit;
+  const { recipient, message } = partialDeposit;
+
+  const fill: v3Fill = {
+    ...partialDeposit,
+    realizedLpFeePct: deposit.realizedLpFeePct ?? bnZero,
+    relayer,
+
+    // Caller can modify these later.
+    exclusiveRelayer: relayer,
+    repaymentChainId: deposit.destinationChainId,
+    updatableRelayData: {
+      recipient: deposit.updatedRecipient ?? recipient,
+      message: deposit.updatedMessage ?? message,
+      outputAmount: deposit.updatedOutputAmount ?? deposit.outputAmount,
+      fillType: FillType.FastFill,
+    },
+  };
+
+  return fill;
+}
+
+
 export function refundRequestFromFill(fill: FillWithBlock, refundToken: string): RefundRequest {
+  const amount = isV2Fill(fill) ? fill.amount : fill.inputAmount;
   const refundRequest: RefundRequest = {
-    amount: fill.amount,
+    amount,
     depositId: fill.depositId,
     originChainId: fill.originChainId,
     destinationChainId: fill.destinationChainId,
@@ -45,7 +86,7 @@ export function refundRequestFromFill(fill: FillWithBlock, refundToken: string):
     fillBlock: toBN(fill.blockNumber),
     relayer: fill.relayer,
     refundToken,
-    previousIdenticalRequests: toBN(0),
+    previousIdenticalRequests: bnZero,
   };
 
   return refundRequest;
