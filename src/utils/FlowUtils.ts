c9b5f8e24843cc8483ac281c5106b6725a2a0086
Paul <108695806+pxrl@users.noreply.github.com>
2024-01-31 11:50:51 +0100

    Additional UBA cleanup

--- src/utils/FlowUtils.ts
+++ src/utils/FlowUtils.ts
@@ -1,62 +1,29 @@
-import assert from "assert";
 import { HubPoolClient } from "../clients/HubPoolClient";
-import { Deposit, Fill, UbaFlow, isUbaInflow, outflowIsFill } from "../interfaces";
-import { BN } from "../utils";
-import { getDepositInputAmount, getFillOutputAmount, isV2Deposit, isV3Deposit, isV2Fill, isV3Fill } from "./V3Utils";
+import { Deposit, Fill, FillWithBlock, UbaFlow, isUbaInflow, outflowIsFill } from "../interfaces";
 
 export const FILL_DEPOSIT_COMPARISON_KEYS = [
-  "depositId",
+  "amount",
   "originChainId",
-  "destinationChainId",
+  "relayerFeePct",
+  "realizedLpFeePct",
+  "depositId",
   "depositor",
   "recipient",
+  "destinationChainId",
+  "destinationToken",
   "message",
 ] as const;
 
-export const V2_DEPOSIT_COMPARISON_KEYS = [
-  ...FILL_DEPOSIT_COMPARISON_KEYS,
-  "amount",
-  "destinationToken",
-  "relayerFeePct",
-  "realizedLpFeePct",
-] as const;
-
-export const V3_DEPOSIT_COMPARISON_KEYS = [
-  ...FILL_DEPOSIT_COMPARISON_KEYS,
-  "inputToken",
-  "inputAmount",
-  "outputToken",
-  "outputAmount",
-] as const;
-
 export function filledSameDeposit(fillA: Fill, fillB: Fill): boolean {
-  if (isV2Fill(fillA) && isV2Fill(fillB)) {
-    return (
-      fillA.depositId === fillB.depositId &&
-      fillA.originChainId === fillB.originChainId &&
-      fillA.amount.eq(fillB.amount) &&
-      fillA.destinationChainId === fillB.destinationChainId &&
-      fillA.relayerFeePct.eq(fillB.relayerFeePct) &&
-      fillA.recipient === fillB.recipient &&
-      fillA.depositor === fillB.depositor &&
-      fillA.message === fillB.message
-    );
-  } else if (isV3Fill(fillA) && isV3Fill(fillB)) {
-    return (
-      fillA.depositId === fillB.depositId &&
-      fillA.originChainId === fillB.originChainId &&
-      fillA.destinationChainId === fillB.destinationChainId &&
-      fillA.recipient === fillB.recipient &&
-      fillA.depositor === fillB.depositor &&
-      fillA.inputToken === fillB.inputToken &&
-      fillA.outputToken === fillB.outputToken &&
-      fillA.message === fillB.message &&
-      fillA.inputAmount.eq(fillB.inputAmount) &&
-      fillA.outputAmount.eq(fillB.outputAmount)
-    );
-  }
-
-  return false;
+  return (
+    fillA.depositId === fillB.depositId &&
+    fillA.originChainId === fillB.originChainId &&
+    fillA.amount.eq(fillB.amount) &&
+    fillA.destinationChainId === fillB.destinationChainId &&
+    fillA.relayerFeePct.eq(fillB.relayerFeePct) &&
+    fillA.recipient === fillB.recipient &&
+    fillA.depositor === fillB.depositor
+  );
 }
 
 // Ensure that each deposit element is included with the same value in the fill. This includes all elements defined
@@ -66,23 +33,13 @@ export function validateFillForDeposit(fill: Fill, deposit?: Deposit, fillFields
     return false;
   }
 
-  if (isV2Deposit(deposit) && isV2Fill(fill)) {
-    return V2_DEPOSIT_COMPARISON_KEYS.every((key) => {
-      if (fillFieldsToIgnore.includes(key)) {
-        return true;
-      }
-      return fill[key] !== undefined && fill[key].toString() === deposit[key]?.toString();
-    });
-  }
-
-  if (isV3Deposit(deposit) && isV3Fill(fill)) {
-    return V3_DEPOSIT_COMPARISON_KEYS.every((key) => {
-      if (fillFieldsToIgnore.includes(key)) {
-        return true;
-      }
-      return fill[key] !== undefined && fill[key].toString() === deposit[key]?.toString();
-    });
-  }
-
-  return false;
+  // Note: this short circuits when a key is found where the comparison doesn't match.
+  // TODO: if we turn on "strict" in the tsconfig, the elements of FILL_DEPOSIT_COMPARISON_KEYS will be automatically
+  // validated against the fields in Fill and Deposit, generating an error if there is a discrepency.
+  return FILL_DEPOSIT_COMPARISON_KEYS.every((key) => {
+    if (fillFieldsToIgnore.includes(key)) {
+      return true;
+    }
+    return fill[key] !== undefined && fill[key].toString() === deposit[key]?.toString();
+  });
 }
