2ce485b54b4f11a930ca3dc4efd0934b7dc4cbf2
Paul <108695806+pxrl@users.noreply.github.com>
2024-02-07 03:42:23 +0100

    Fix previous

--- src/clients/SpokePoolClient.ts
+++ src/clients/SpokePoolClient.ts
@@ -124,10 +124,10 @@ export class SpokePoolClient extends BaseAbstractClient {
       RelayedRootBundle: this.spokePool.filters.RelayedRootBundle(),
       ExecutedRelayerRefundRoot: this.spokePool.filters.ExecutedRelayerRefundRoot(),
       // These events will only work after bumping to the new contracts-v2 package.
-      V3FundsDeposited: this.spokePool.filters.V3FundsDeposited(),
-      RequestedSpeedUpV3Deposit: this.spokePool.filters.RequestedSpeedUpV3Deposit(),
-      FilledV3Relay: this.spokePool.filters.FilledV3Relay(),
-      ExecutedV3RelayerRefundRoot: this.spokePool.filters.ExecutedV3RelayerRefundRoot(),
+      // V3FundsDeposited: this.spokePool.filters.V3FundsDeposited(),
+      // RequestedSpeedUpV3Deposit: this.spokePool.filters.RequestedSpeedUpV3Deposit(),
+      // FilledV3Relay: this.spokePool.filters.FilledV3Relay(),
+      // ExecutedV3RelayerRefundRoot: this.spokePool.filters.ExecutedV3RelayerRefundRoot(),
     };
   }
 
