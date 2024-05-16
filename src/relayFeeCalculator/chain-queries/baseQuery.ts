dda1ccb9b9de54d9e64bf21fbbcedff97b9ca962
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-05-16 09:52:28 -0400

    chore(refactor): relayfee calculator queries

--- src/relayFeeCalculator/chain-queries/baseQuery.ts
+++ src/relayFeeCalculator/chain-queries/baseQuery.ts
@@ -2,15 +2,20 @@ import { SpokePool, SpokePool__factory } from "../../typechain";
 import { L2Provider } from "@eth-optimism/sdk/dist/interfaces/l2-provider";
 import { providers } from "ethers";
 import { Coingecko } from "../../coingecko";
-import { CHAIN_IDs, DEFAULT_SIMULATED_RELAYER_ADDRESS } from "../../constants";
+import { CHAIN_IDs, DEFAULT_SIMULATED_RELAYER_ADDRESS, TOKEN_SYMBOLS_MAP } from "../../constants";
 import {
   BigNumberish,
+  chainIsMatic,
+  chainIsOPStack,
   estimateTotalGasRequiredByUnsignedTransaction,
+  getDeployedAddress,
   populateV3Relay,
   TransactionCostEstimate,
 } from "../../utils";
-import { Logger, QueryInterface } from "../relayFeeCalculator";
+import { DEFAULT_LOGGER, Logger, QueryInterface } from "../relayFeeCalculator";
 import { Deposit } from "../../interfaces";
+import { asL2Provider } from "@eth-optimism/sdk";
+import { PolygonQueries } from "./polygon";
 
 type Provider = providers.Provider;
 type OptimismProvider = L2Provider<Provider>;
@@ -26,14 +31,13 @@ type SymbolMappingType = Record<
  * A unified QueryBase for querying gas costs, token prices, and decimals of various tokens
  * on a blockchain.
  */
-export default abstract class QueryBase implements QueryInterface {
+export default class QueryBase implements QueryInterface {
   readonly spokePool: SpokePool;
   /**
    * Instantiates a QueryBase instance
    * @param provider A valid Ethers.js provider
    * @param symbolMapping A mapping to valid ERC20 tokens and their respective characteristics
    * @param spokePoolAddress The valid address of the Spoke Pool deployment
-   * @param usdcAddress The valid token address of the USDC ERC-20 token
    * @param simulatedRelayerAddress The address that these queries will reference as the sender. Note: This address must be approved for USDC
    * @param gasMarkup A multiplier that is applied to the total gas estimate
    * @param logger A logging utility to report logs
@@ -41,7 +45,7 @@ export default abstract class QueryBase implements QueryInterface {
    * @param fixedGasPrice Overrides the gas price with a fixed value. Note: primarily used for the Boba blockchain
    * @param coingeckoBaseCurrency The basis currency that CoinGecko will use to resolve pricing
    */
-  protected constructor(
+  constructor(
     readonly provider: Provider | OptimismProvider,
     readonly symbolMapping: SymbolMappingType,
     readonly spokePoolAddress: string,
@@ -95,5 +99,54 @@ export default abstract class QueryBase implements QueryInterface {
   getTokenDecimals(tokenSymbol: string): number {
     if (!this.symbolMapping[tokenSymbol]) throw new Error(`${tokenSymbol} does not exist in mapping`);
     return this.symbolMapping[tokenSymbol].decimals;
+  }
+}
+
+/**
+ * Some chains have a fixed gas price that is applied to the gas estimates. We should override
+ * the gas markup for these chains.
+ */
+const fixedGasPrice = {
+  [CHAIN_IDs.BOBA]: 1e9,
+};
+
+export class QueryBase__factory {
+  static create(
+    chainId: number,
+    provider: providers.Provider,
+    symbolMapping = TOKEN_SYMBOLS_MAP,
+    spokePoolAddress = getDeployedAddress("SpokePool", chainId),
+    simulatedRelayerAddress = DEFAULT_SIMULATED_RELAYER_ADDRESS,
+    coingeckoProApiKey?: string,
+    logger: Logger = DEFAULT_LOGGER,
+    gasMarkup = 0,
+    coingeckoBaseCurrency = "eth"
+  ): QueryBase {
+    // Currently the only chain that has a custom query class is Polygon
+    if (chainIsMatic(chainId)) {
+      return new PolygonQueries(
+        provider,
+        symbolMapping,
+        spokePoolAddress,
+        simulatedRelayerAddress,
+        coingeckoProApiKey,
+        logger,
+        gasMarkup
+      );
+    }
+    // For OPStack chains, we need to wrap the provider in an L2Provider
+    provider = chainIsOPStack(chainId) ? asL2Provider(provider) : provider;
+
+    return new QueryBase(
+      provider,
+      symbolMapping,
+      spokePoolAddress,
+      simulatedRelayerAddress,
+      gasMarkup,
+      logger,
+      coingeckoProApiKey,
+      fixedGasPrice[chainId],
+      coingeckoBaseCurrency
+    );
   }
 }
