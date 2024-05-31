cea26c21d35a1d76fc3405bf0b8ec782eb7b767f
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-01 00:05:32 +0200

    lint

--- test/SpokePoolClient.v3Events.ts
+++ test/SpokePoolClient.v3Events.ts
@@ -6,7 +6,7 @@ import { DEFAULT_CONFIG_STORE_VERSION } from "../src/clients";
 import { MockHubPoolClient, MockSpokePoolClient, MockConfigStoreClient } from "../src/clients/mocks";
 import { DepositWithBlock, FillWithBlock, SlowFillRequest, SlowFillRequestWithBlock } from "../src/interfaces";
 import { EMPTY_MESSAGE, ZERO_ADDRESS } from "../src/constants";
-import { getCurrentTime, getRelayDataHash, isDefined, randomAddress } from "../src/utils";
+import { getCurrentTime, isDefined, randomAddress } from "../src/utils";
 import {
   createSpyLogger,
   fillFromDeposit,
