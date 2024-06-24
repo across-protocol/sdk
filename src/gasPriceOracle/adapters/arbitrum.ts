c79926ff63d708d975dd149dafe9bf51f7d15496
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-24 14:51:08 +0200

    improve(gasPriceOracle): Updates & add Linea
    - Minor refactorings & simplifications.
    - Tigher scoping for adapter exports.
    - Remove unnecessary async/await statements.
    - Add fallback strategy for OP stack implementations that are not
      manually defined. This eases new-chain integrations.
    
    While here, establish a placeholder for a custom Linea gas pricing
    strategy, to be supported once it is made available on mainnet.

--- src/gasPriceOracle/adapters/arbitrum.ts
+++ src/gasPriceOracle/adapters/arbitrum.ts
@@ -1,21 +1,24 @@
-import { ethers, providers, utils as ethersUtils } from "ethers";
+import { providers, utils as ethersUtils } from "ethers";
+import { bnOne } from "../../utils";
 import { GasPriceEstimate } from "../types";
-import { eip1559 } from "./ethereum";
+import * as ethereum from "./ethereum";
+
+const DEFAULT_PRIORITY_FEE = ethersUtils.parseUnits("1.5", 9);
 
 // Arbitrum Nitro implements EIP-1559 pricing, but the priority fee is always refunded to the caller. Further,
 // ethers typically hardcodes the priority fee to 1.5 Gwei. So, confirm that the priority fee supplied was 1.5
 // Gwei, and then drop it to 1 Wei. Reference: https://developer.arbitrum.io/faqs/gas-faqs#q-priority
-export async function eip1559_arbitrum(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
-  const { maxFeePerGas: _maxFeePerGas, maxPriorityFeePerGas } = await eip1559(provider, chainId);
+export async function eip1559(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
+  const { maxFeePerGas: _maxFeePerGas, maxPriorityFeePerGas } = await ethereum.eip1559(provider, chainId);
 
   // If this throws, ethers default behaviour has changed, or Arbitrum RPCs are returning something more sensible.
-  if (!maxPriorityFeePerGas.eq(ethersUtils.parseUnits("1.5", 9))) {
+  if (!maxPriorityFeePerGas.eq(DEFAULT_PRIORITY_FEE)) {
     throw new Error(`Expected hardcoded 1.5 Gwei priority fee on Arbitrum, got ${maxPriorityFeePerGas}`);
   }
 
   // eip1559() sets maxFeePerGas = lastBaseFeePerGas + maxPriorityFeePerGas, so revert that.
   // The caller may apply scaling as they wish afterwards.
-  const maxFeePerGas = _maxFeePerGas.sub(maxPriorityFeePerGas).add(1);
+  const maxFeePerGas = _maxFeePerGas.sub(maxPriorityFeePerGas).add(bnOne);
 
-  return { maxPriorityFeePerGas: ethers.constants.One, maxFeePerGas };
+  return { maxPriorityFeePerGas: bnOne, maxFeePerGas };
 }
