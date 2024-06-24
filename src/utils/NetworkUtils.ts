262d126c586fe3f0cd01f07292f0c1a0d82dca98
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-24 12:29:51 +0200

    feat: Add Lisk definitions

--- src/utils/NetworkUtils.ts
+++ src/utils/NetworkUtils.ts
@@ -60,11 +60,11 @@ export function chainIsOPStack(chainId: number): boolean {
   return [
     CHAIN_IDs.OPTIMISM,
     CHAIN_IDs.BASE,
+    CHAIN_IDs.LISK,
     CHAIN_IDs.MODE,
-    CHAIN_IDs.OPTIMISM_GOERLI,
-    CHAIN_IDs.BASE_GOERLI,
     CHAIN_IDs.OPTIMISM_SEPOLIA,
     CHAIN_IDs.BASE_SEPOLIA,
+    CHAIN_IDs.LISK_SEPOLIA,
     CHAIN_IDs.MODE_SEPOLIA,
   ].includes(chainId);
 }
