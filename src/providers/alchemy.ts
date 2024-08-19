95b54bb0503c9fa17122b36123b12170ae2c96e3
Paul <108695806+pxrl@users.noreply.github.com>
2024-08-20 00:10:58 +0200

    fixes

--- src/providers/alchemy.ts
+++ src/providers/alchemy.ts
@@ -1,18 +1,25 @@
-import { CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
+import { CHAIN_IDs, MAINNET_CHAIN_IDs as _MAINNET_CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
 import { RPCTransport } from "./types";
+
+const MAINNET_CHAIN_IDs = Object.values(_MAINNET_CHAIN_IDs);
 
 // Chain-specific overrides for when the Alchemy endpoint does not match the canonical chain name.
 const endpoints: { [chainId: string]: string } = {
-  [CHAIN_IDs.ARBITRUM]: "arb-mainnet",
-  [CHAIN_IDs.MAINNET]: "eth-mainnet",
-  [CHAIN_IDs.OPTIMISM]: "opt-mainnet",
+  [CHAIN_IDs.ARBITRUM]: "arb",
+  [CHAIN_IDs.MAINNET]: "eth",
+  [CHAIN_IDs.OPTIMISM]: "opt",
 };
 
 export function getURL(chainId: number, apiKey: string, transport: RPCTransport): string {
-  const host = endpoints[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
+  let host = endpoints[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
   if (!host) {
     throw new Error(`No known Alchemy provider for chainId ${chainId}`);
   }
+
+  if (MAINNET_CHAIN_IDs.includes(chainId)) {
+    host = `${host}-mainnet`;
+  }
+  host = host.toLowerCase().replace(" ", "-");
 
   return `${transport.toLowerCase()}://${host.toLowerCase().replace(" ", "-")}.g.alchemy.com/v2/${apiKey}`;
 }
--- src/providers/infura.ts
+++ src/providers/infura.ts
@@ -1,10 +1,12 @@
-import { MAINNET_CHAIN_IDs as _MAINNET_CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
+import { CHAIN_IDs, MAINNET_CHAIN_IDs as _MAINNET_CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
 import { RPCTransport } from "./types";
 
 const MAINNET_CHAIN_IDs = Object.values(_MAINNET_CHAIN_IDs);
 
 // Chain-specific overrides for when the Infura endpoint does not match the canonical chain name.
-const endpoints: { [chainId: string]: string } = {};
+const endpoints: { [chainId: string]: string } = {
+  [CHAIN_IDs.ARBITRUM]: "arbitrum",
+};
 
 export function getURL(chainId: number, apiKey: string, transport: RPCTransport): string {
   let host = endpoints[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
@@ -12,10 +14,10 @@ export function getURL(chainId: number, apiKey: string, transport: RPCTransport)
     throw new Error(`No known Infura provider for chainId ${chainId}`);
   }
 
-  if (MAINNET_CHAIN_IDs.includes(chainId)) {
+  if (chainId !== CHAIN_IDs.MAINNET && MAINNET_CHAIN_IDs.includes(chainId)) {
     host = `${host}-mainnet`;
   }
   host = host.toLowerCase().replace(" ", "-");
 
-  return transport === "https" ? `https://${host}.infura.io/v3/${apiKey}` : `wss://${host}.infura.is/ws/v3/${apiKey}`;
+  return transport === "https" ? `https://${host}.infura.io/v3/${apiKey}` : `wss://${host}.infura.io/ws/v3/${apiKey}`;
 }
