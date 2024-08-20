d5a5f6d870e85238a8aa398e3c06a6854d103fd1
Paul <108695806+pxrl@users.noreply.github.com>
2024-08-20 18:10:35 +0200

    feat: Cheat codes for resolving provider URLs (#704)

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,4 +2,5 @@ export * from "./rateLimitedProvider";
 export * from "./cachedProvider";
 export * from "./retryProvider";
 export * from "./constants";
+export * from "./types";
 export * from "./utils";
