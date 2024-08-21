e5c8b224ae81358becf3956a78bd708f9fe260df
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-08-21 11:34:12 -0400

    add: ternary level helper functions
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

---
+++ src/clients/BundleDataClient/utils/MerkleTreeUtils.ts
@@ -0,0 +1,21 @@
+export function buildPoolRebalanceLeafTree(poolRebalanceLeaves: PoolRebalanceLeaf[]): MerkleTree<PoolRebalanceLeaf> {
+  for (let i = 0; i < poolRebalanceLeaves.length; i++) {
+    // The 4 provided parallel arrays must be of equal length. Running Balances can optionally be 2x the length
+    if (
+      poolRebalanceLeaves[i].l1Tokens.length !== poolRebalanceLeaves[i].bundleLpFees.length ||
+      poolRebalanceLeaves[i].netSendAmounts.length !== poolRebalanceLeaves[i].bundleLpFees.length
+    ) {
+      throw new Error("Provided lef arrays are not of equal length");
+    }
+    if (
+      poolRebalanceLeaves[i].runningBalances.length !== poolRebalanceLeaves[i].bundleLpFees.length * 2 &&
+      poolRebalanceLeaves[i].runningBalances.length !== poolRebalanceLeaves[i].bundleLpFees.length
+    ) {
+      throw new Error("Running balances length unexpected");
+    }
+  }
+
+  const paramType = getParamType("MerkleLibTest", "verifyPoolRebalance", "rebalance");
+  const hashFn = (input: PoolRebalanceLeaf) => utils.keccak256(utils.defaultAbiCoder.encode([paramType], [input]));
+  return new MerkleTree<PoolRebalanceLeaf>(poolRebalanceLeaves, hashFn);
+}
---
+++ src/utils/ContractUtils.ts
@@ -0,0 +1,5 @@
+export function getParamType(contractName: string, functionName: string, paramName: string): string {
+  const artifact = typechain[`${[contractName]}__factory`];
+  const fragment = artifact.abi.find((fragment: { name: string }) => fragment.name === functionName);
+  return fragment.inputs.find((input: { name: string }) => input.name === paramName) || "";
+}
