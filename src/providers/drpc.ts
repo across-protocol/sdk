c2bb86d3ff19616373fa1b2463d43a0ef362fb61
Paul <108695806+pxrl@users.noreply.github.com>
2024-08-22 09:51:51 +0200

    update

--- src/providers/drpc.ts
+++ src/providers/drpc.ts
@@ -1,19 +1,19 @@
 import { CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
 import { RPCTransport } from "./types";
 
-// Chain-specific overrides for when the DRPC endpoint name does not match the canonical chain name.
+// Chain-specific overrides for when the endpoint name does not match the canonical chain name.
 const endpoints: { [chainId: string]: string } = {
   [CHAIN_IDs.ARBITRUM]: "arbitrum",
   [CHAIN_IDs.MAINNET]: "ethereum",
 };
 
 export function getURL(chainId: number, apiKey: string, transport: RPCTransport): string {
-  let host = endpoints[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
-  if (!host) {
+  let chain = endpoints[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
+  if (!chain) {
     throw new Error(`No known DRPC provider for chainId ${chainId}`);
   }
-  host = host.toLowerCase().replace(" ", "-");
+  chain = chain.toLowerCase().replace(" ", "-");
   const rpcType = transport === "https" ? "rpc" : "ws";
 
-  return `${transport}://lb.drpc.org/og${rpcType}?network=${host}&dkey=${apiKey}`;
+  return `${transport}://lb.drpc.org/og${rpcType}?network=${chain}&dkey=${apiKey}`;
 }
