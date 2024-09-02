710a5c1c54ed02ef9f9752295afb27cf67279cb7
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-09-02 18:12:23 +0700

    feat: allow passing in `gasUnit` to `getGasCosts`

--- src/relayFeeCalculator/chain-queries/baseQuery.ts
+++ src/relayFeeCalculator/chain-queries/baseQuery.ts
@@ -60,13 +60,15 @@ export class QueryBase implements QueryInterface {
    * @param relayerAddress Relayer address to simulate with.
    * @param gasPrice Optional gas price to use for the simulation.
    * @param gasLimit Optional gas limit to use for the simulation.
+   * @param gasUnits Optional gas units to use for the simulation.
    * @returns The gas estimate for this function call (multiplied with the optional buffer).
    */
   async getGasCosts(
     deposit: Deposit,
     relayer = DEFAULT_SIMULATED_RELAYER_ADDRESS,
     gasPrice = this.fixedGasPrice,
-    gasLimit?: BigNumberish
+    gasLimit?: BigNumberish,
+    gasUnits?: BigNumberish
   ): Promise<TransactionCostEstimate> {
     const tx = await populateV3Relay(this.spokePool, deposit, relayer);
     return estimateTotalGasRequiredByUnsignedTransaction(
@@ -75,7 +77,8 @@ export class QueryBase implements QueryInterface {
       this.provider,
       this.gasMarkup,
       gasPrice,
-      gasLimit
+      gasLimit,
+      gasUnits
     );
   }
 
