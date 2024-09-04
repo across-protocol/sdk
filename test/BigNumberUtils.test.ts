19d09c2bbbdfb980acb0fc269d0d60400acf677e
Paul <108695806+pxrl@users.noreply.github.com>
2024-09-05 00:52:54 +0200

    Cleanup test

--- test/BigNumberUtils.test.ts
+++ test/BigNumberUtils.test.ts
@@ -1,12 +1,11 @@
-import { ethers } from "ethers";
-import { toWei, toGWei, toBN } from "../src/utils/BigNumberUtils";
+import { BigNumber, parseEther, parseUnits, toWei, toGWei, toBN } from "../src/utils/BigNumberUtils";
 import { expect } from "./utils";
 
 describe("BigNumberUtils", () => {
   describe("toWei", () => {
     it("should convert a stringified number to a BigNumber with 18 decimal places", () => {
       const num = "123.456";
-      const expected = ethers.utils.parseEther(num);
+      const expected = parseEther(num);
       const result = toWei(num);
       expect(result).to.be.deep.includes(expected);
     });
@@ -15,7 +14,7 @@ describe("BigNumberUtils", () => {
   describe("toGWei", () => {
     it("should convert a stringified number to a BigNumber with 9 decimal places", () => {
       const num = "123.456";
-      const expected = ethers.utils.parseUnits(num, 9);
+      const expected = parseUnits(num, 9);
       const result = toGWei(num);
       expect(result).to.be.deep.includes(expected);
     });
@@ -24,28 +23,28 @@ describe("BigNumberUtils", () => {
   describe("toBN", () => {
     it("should convert a stringified integer to a BigNumber", () => {
       const num = "123456";
-      const expected = ethers.BigNumber.from(num);
+      const expected = BigNumber.from(num);
       const result = toBN(num);
       expect(result).to.be.deep.includes(expected);
     });
 
     it("should convert a stringified number with decimal places to a BigNumber and round down by default", () => {
       const num = "123.456";
-      const expected = ethers.BigNumber.from("123");
+      const expected = BigNumber.from("123");
       const result = toBN(num);
       expect(result).to.be.deep.includes(expected);
     });
 
     it("should round up if rounding is set to 'ceil'", () => {
       const num = "123.456";
-      const expected = ethers.BigNumber.from("124");
+      const expected = BigNumber.from("124");
       const result = toBN(num, "ceil");
       expect(result).to.be.deep.includes(expected);
     });
 
     it("should round up if rounding is set to 'round' and the first decimal is greater than or equal to 5", () => {
       const num = "123.556";
-      const expected = ethers.BigNumber.from("124");
+      const expected = BigNumber.from("124");
       const result = toBN(num, "round");
       expect(result).to.be.deep.includes(expected);
     });
