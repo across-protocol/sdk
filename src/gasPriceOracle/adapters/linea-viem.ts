2fc230d7ec32fa78a56524cf3017f83843306933
Paul <108695806+pxrl@users.noreply.github.com>
2024-11-26 11:13:07 +0000

    Update

--- src/gasPriceOracle/adapters/linea-viem.ts
+++ src/gasPriceOracle/adapters/linea-viem.ts
@@ -1,12 +1,17 @@
 import { PublicClient } from "viem";
 import { InternalGasPriceEstimate } from "../types";
+import { createPublicClient } from 'viem'
+import { linea } from 'viem/chains'
+import { estimateGas } from 'viem/linea'
 
 export async function eip1559(provider: PublicClient, _chainId: number): Promise<InternalGasPriceEstimate> {
   const account = "0x...";
 
+	const lineaClient = createPublicClient({ chain: linea, transport: provider.transport });
+
   // @todo: Coax viem into recognising that this is a Linea-specific provider.
   // https://github.com/wevm/viem/blob/main/src/linea/actions/estimateGas.ts
-  const { baseFeePerGas, priorityFeePerGas } = await provider.estimateGas({
+  const { baseFeePerGas, priorityFeePerGas } = await estimateGas(lineaClient, {
     account,
     to: account,
     value: BigInt(1),
