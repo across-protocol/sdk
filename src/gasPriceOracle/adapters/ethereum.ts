750186faac0053c840012ca182a146c4489f0172
Paul <108695806+pxrl@users.noreply.github.com>
2024-12-21 00:11:19 +0100

    improve(gasPriceOracle): Dynamic priority fee on EIP 1559 queries (#803)
    This change introduces a new "raw" variant of the gasPriceOracle
    EIP-1559 adadpter. This new adapter calls the eth_maxPriorityFeePerGas
    method in parallel with querying the gas price of the pending block.
    This is intended as a replacement for the ethers getFeeData() query,
    which resolves the base fee by querying the latest block, but annoyingly
    hardcodes the priority fee to 1.5 Gwei.
    
    Testing on Optimism shows that this returns a fixed fee of 0xf4240. This
    is however returned by the RPC endpoint, rather than determined by
    ethers itself.
    
    Testing on ethereum also shows a lower priority fee vs. vanilla ethers
    v5 with its fixed 1.5 Gwei priority fee.
    
    The raw adapter can be enabled via setting the environment variable:
    GAS_PRICE_EIP1559_RAW_=true

--- src/gasPriceOracle/adapters/ethereum.ts
+++ src/gasPriceOracle/adapters/ethereum.ts
@@ -1,9 +1,46 @@
+import assert from "assert";
 import { providers } from "ethers";
-import { BigNumber, bnZero } from "../../utils";
+import { BigNumber, bnZero, getNetworkName } from "../../utils";
 import { GasPriceEstimate } from "../types";
 import { gasPriceError } from "../util";
 
-export async function eip1559(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
+/**
+ * @param provider ethers RPC provider instance.
+ * @param chainId Chain ID of provider instance.
+ * @returns Promise of gas price estimate object.
+ */
+export function eip1559(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
+  const useRaw = process.env[`GAS_PRICE_EIP1559_RAW_${chainId}`] === "true";
+  return useRaw ? eip1559Raw(provider, chainId) : eip1559Bad(provider, chainId);
+}
+
+/**
+ * @note Performs direct RPC calls to retrieve the RPC-suggested priority fee for the next block.
+ * @param provider ethers RPC provider instance.
+ * @param chainId Chain ID of the provider instance.
+ * @returns Promise of gas price estimate object.
+ */
+export async function eip1559Raw(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
+  const [{ baseFeePerGas }, _maxPriorityFeePerGas] = await Promise.all([
+    provider.getBlock("pending"),
+    (provider as providers.JsonRpcProvider).send("eth_maxPriorityFeePerGas", []),
+  ]);
+  const maxPriorityFeePerGas = BigNumber.from(_maxPriorityFeePerGas);
+  assert(BigNumber.isBigNumber(baseFeePerGas), `No baseFeePerGas received on ${getNetworkName(chainId)}`);
+
+  return {
+    maxFeePerGas: maxPriorityFeePerGas.add(baseFeePerGas),
+    maxPriorityFeePerGas,
+  };
+}
+
+/**
+ * @note Resolves priority gas pricing poorly, because the priority fee is hardcoded to 1.5 Gwei in ethers v5.
+ * @param provider ethers RPC provider instance.
+ * @param chainId Chain ID of the provider instance.
+ * @returns Promise of gas price estimate object.
+ */
+export async function eip1559Bad(provider: providers.Provider, chainId: number): Promise<GasPriceEstimate> {
   const feeData = await provider.getFeeData();
 
   [feeData.lastBaseFeePerGas, feeData.maxPriorityFeePerGas].forEach((field: BigNumber | null) => {
