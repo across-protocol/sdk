668077395709df29d76e47c777e40f0ec8af0827
Paul <108695806+pxrl@users.noreply.github.com>
2025-01-31 13:02:25 +0000

    lint

--- test/SpokeUtils.ts
+++ test/SpokeUtils.ts
@@ -3,7 +3,7 @@ import { getMessageHash, keccak256 } from "../src/utils";
 import { expect } from "./utils";
 
 describe("SpokeUtils", function () {
-  it("getMessageHash correctly handles empty messages", async function () {
+  it("getMessageHash correctly handles empty messages", function () {
     expect(getMessageHash("")).to.equal(ZERO_BYTES);
     expect(getMessageHash("0x")).to.equal(ZERO_BYTES);
     expect(getMessageHash("0x1234")).to.equal(keccak256("0x1234"));
