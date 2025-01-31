d051eaf006b67d24a1571dfc243c1dc382da0dee
Paul <108695806+pxrl@users.noreply.github.com>
2025-01-31 12:57:57 +0000

    test

---
+++ test/SpokeUtils.ts
@@ -0,0 +1,11 @@
+import { ZERO_BYTES } from "../src/constants";
+import { getMessageHash, keccak256 } from "../src/utils";
+import { expect } from "./utils";
+
+describe("SpokeUtils", function () {
+  it("getMessageHash correctly handles empty messages", async function () {
+    expect(getMessageHash("")).to.equal(ZERO_BYTES);
+    expect(getMessageHash("0x")).to.equal(ZERO_BYTES);
+    expect(getMessageHash("0x1234")).to.equal(keccak256("0x1234"));
+  });
+});
