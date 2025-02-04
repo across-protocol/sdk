7e61ac34e246dcbba60c536cfe75e7d6682cbbb2
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-04 20:47:45 +0000

    lint

--- test/SpokePoolClient.v3Events.ts
+++ test/SpokePoolClient.v3Events.ts
@@ -547,7 +547,7 @@ describe("SpokePoolClient: Event Filtering", function () {
 
       destinationSpokePoolClient.requestV3SlowFill({
         ...deposit,
-        blockNumber: destinationSpokePoolClient.latestBlockSearched
+        blockNumber: destinationSpokePoolClient.latestBlockSearched,
       });
       await destinationSpokePoolClient.update();
 
