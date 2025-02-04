cb694a7393a1a365475b55630af251c368fc056d
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-04 23:52:38 +0100

    refactor: Relocate event-to-string utility function (#860)
    This functionality can be used in more places than just the 
    BundleDataClient. Refactor it out and add a simple test.

--- test/SpokeUtils.ts
+++ test/SpokeUtils.ts
@@ -1,11 +1,54 @@
+import { utils as ethersUtils } from "ethers";
 import { ZERO_BYTES } from "../src/constants";
-import { getMessageHash, keccak256 } from "../src/utils";
+import { getMessageHash, getRelayEventKey, keccak256, randomAddress, toBN } from "../src/utils";
 import { expect } from "./utils";
 
+const random = () => Math.round(Math.random() * 1e8);
+
 describe("SpokeUtils", function () {
+  it("getRelayEventKey correctly concatenates an event key", function () {
+    const data = {
+      originChainId: random(),
+      destinationChainId: random(),
+      depositor: randomAddress(),
+      recipient: randomAddress(),
+      inputToken: randomAddress(),
+      inputAmount: toBN(random()),
+      outputToken: randomAddress(),
+      outputAmount: toBN(random()),
+      message: `0x${ethersUtils.randomBytes(48).join("")}`,
+      depositId: toBN(random()),
+      fillDeadline: random(),
+      exclusiveRelayer: randomAddress(),
+      exclusivityDeadline: random(),
+    };
+
+    const eventKey = getRelayEventKey(data);
+    const expectedKey =
+      `${data.depositor}` +
+      `-${data.recipient}` +
+      `-${data.exclusiveRelayer}` +
+      `-${data.inputToken}` +
+      `-${data.outputToken}` +
+      `-${data.inputAmount}` +
+      `-${data.outputAmount}` +
+      `-${data.originChainId}` +
+      `-${data.destinationChainId}` +
+      `-${data.depositId}` +
+      `-${data.fillDeadline}` +
+      `-${data.exclusivityDeadline}` +
+      `-${data.message}`;
+
+    expect(eventKey).to.equal(expectedKey);
+    eventKey.split("-").forEach((field) => expect(field).to.not.equal("undefined"));
+  });
+
   it("getMessageHash correctly handles empty messages", function () {
     expect(getMessageHash("")).to.equal(ZERO_BYTES);
     expect(getMessageHash("0x")).to.equal(ZERO_BYTES);
     expect(getMessageHash("0x1234")).to.equal(keccak256("0x1234"));
+
+    const message = `0x${ethersUtils.randomBytes(48).join("")}`;
+    expect(getMessageHash(message)).to.equal(keccak256(message));
   });
 });
