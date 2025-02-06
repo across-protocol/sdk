8f998f3aa407e634d08879785592ea7d377288af
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-06 09:04:12 +0000

    chore(SpokePoolClient): Backport changes
    Diff minimisation to reduce the size of the subsequent diff for the pending
    contract upgrade.

--- src/clients/SpokePoolClient.ts
+++ src/clients/SpokePoolClient.ts
@@ -15,6 +15,7 @@ import {
   bnOne,
   getMessageHash,
   isUnsafeDepositId,
+  isZeroAddress,
 } from "../utils";
 import {
   paginatedEventQuery,
@@ -629,7 +630,7 @@ export class SpokePoolClient extends BaseAbstractClient {
         deposit.fromLiteChain = this.isOriginLiteChain(deposit);
         deposit.toLiteChain = this.isDestinationLiteChain(deposit);
 
-        if (deposit.outputToken === ZERO_ADDRESS) {
+        if (isZeroAddress(deposit.outputToken)) {
           deposit.outputToken = this.getDestinationTokenForDeposit(deposit);
         }
 
@@ -883,7 +884,7 @@ export class SpokePoolClient extends BaseAbstractClient {
       toLiteChain: true, // To be updated immediately afterwards.
     } as DepositWithBlock;
 
-    if (deposit.outputToken === ZERO_ADDRESS) {
+    if (isZeroAddress(deposit.outputToken)) {
       deposit.outputToken = this.getDestinationTokenForDeposit(deposit);
     }
     deposit.fromLiteChain = this.isOriginLiteChain(deposit);
