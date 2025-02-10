a0971aa5272f0da0d0dbedc6de4bfa8f24e59d5a
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-10 16:14:49 +0100

    Update BaseAbstractClient.test.ts

--- test/BaseAbstractClient.test.ts
+++ test/BaseAbstractClient.test.ts
@@ -8,7 +8,7 @@ describe("Test that the BaseAbstractClient class works as expected", () => {
     const client = new TestAbstractClient();
     expect(client).to.be.instanceOf(TestAbstractClient);
   });
-  it("Test that the isUdpated variable works as expected", () => {
+  it("Test that the isUpdated variable works as expected", () => {
     const client = new TestAbstractClient();
     expect(client.isUpdated).to.not.be.undefined;
     expect(client.isUpdated).to.be.false;
