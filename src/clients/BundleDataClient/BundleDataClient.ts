331dd34d3061be4066e435378a902382b3e55034
nicholaspai <npai.nyc@gmail.com>
2025-02-11 11:44:23 -0500

    Update BundleDataClient.ts

--- src/clients/BundleDataClient/BundleDataClient.ts
+++ src/clients/BundleDataClient/BundleDataClient.ts
@@ -914,7 +914,7 @@ export class BundleDataClient {
           );
           if (deposit.blockNumber >= originChainBlockRange[0]) {
             if (
-              bundleDepositsV3?.[originChainId]?.[deposit.inputToken].find(
+              bundleDepositsV3?.[originChainId]?.[deposit.inputToken]?.find(
                 (d) => d.transactionHash === deposit.transactionHash && d.logIndex === deposit.logIndex
               )
             ) {
