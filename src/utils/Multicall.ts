7890a4a60e0fd4e7920a6864bb8416464ba60641
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2025-02-13 13:56:13 -0500

    Update Multicall.ts

--- src/utils/Multicall.ts
+++ src/utils/Multicall.ts
@@ -34,7 +34,7 @@ const DETERMINISTIC_MULTICALL_CHAINS = [
   CHAIN_IDs.POLYGON_AMOY,
   CHAIN_IDs.SCROLL_SEPOLIA,
   CHAIN_IDs.SEPOLIA,
-  CHAIN_IDS.ARBITRUM_SEPOLIA,
+  CHAIN_IDs.ARBITRUM_SEPOLIA,
 ];
 
 export function getMulticallAddress(chainId: number): string | undefined {
