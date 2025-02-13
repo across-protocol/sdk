dde2cc68104129d39c4776f657a0b73d33223799
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2025-02-13 14:02:05 -0500

    improve: Add ARBITRUM_SEPOLIA to Multicall list (#889)

--- src/utils/Multicall.ts
+++ src/utils/Multicall.ts
@@ -34,6 +34,7 @@ const DETERMINISTIC_MULTICALL_CHAINS = [
   CHAIN_IDs.POLYGON_AMOY,
   CHAIN_IDs.SCROLL_SEPOLIA,
   CHAIN_IDs.SEPOLIA,
+  CHAIN_IDs.ARBITRUM_SEPOLIA,
 ];
 
 export function getMulticallAddress(chainId: number): string | undefined {
