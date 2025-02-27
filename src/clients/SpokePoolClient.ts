88872968ed4c9a3760679e30e2d0686aa70074a3
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-27 21:19:20 +0100

    improve(SpokePoolClient): Suppress duplicate invalid fill logs (#903)
    The fast relayer logs invalid fills on every SpokePoolCLient update.
    This is hugely spammy when the relayer loops quickly.

--- src/clients/SpokePoolClient.ts
+++ src/clients/SpokePoolClient.ts
@@ -84,6 +84,7 @@ export class SpokePoolClient extends BaseAbstractClient {
   protected relayerRefundExecutions: RelayerRefundExecutionWithBlock[] = [];
   protected queryableEventNames: string[] = [];
   protected configStoreClient: AcrossConfigStoreClient | undefined;
+  protected invalidFills: Set<string> = new Set();
   public earliestDepositIdQueried = MAX_BIG_INT;
   public latestDepositIdQueried = bnZero;
   public firstDepositIdForSpokePool = MAX_BIG_INT;
@@ -380,7 +381,7 @@ export class SpokePoolClient extends BaseAbstractClient {
   public getValidUnfilledAmountForDeposit(deposit: Deposit): {
     unfilledAmount: BigNumber;
     fillCount: number;
-    invalidFills: Fill[];
+    invalidFills: FillWithBlock[];
   } {
     const { outputAmount } = deposit;
     const fillsForDeposit = this.depositHashesToFills[this.getDepositHash(deposit)];
@@ -390,7 +391,10 @@ export class SpokePoolClient extends BaseAbstractClient {
     }
 
     const { validFills, invalidFills, unrepayableFills } = fillsForDeposit.reduce(
-      (groupedFills: { validFills: Fill[]; invalidFills: Fill[]; unrepayableFills: Fill[] }, fill: Fill) => {
+      (
+        groupedFills: { validFills: FillWithBlock[]; invalidFills: FillWithBlock[]; unrepayableFills: FillWithBlock[] },
+        fill: FillWithBlock
+      ) => {
         if (validateFillForDeposit(fill, deposit).valid) {
           const repaymentChainId = getRepaymentChainId(fill, deposit);
           // In order to keep this function sync, we can't call verifyFillRepayment so we'll log any fills that
@@ -423,7 +427,15 @@ export class SpokePoolClient extends BaseAbstractClient {
     );
 
     // Log any invalid deposits with same deposit id but different params.
-    const invalidFillsForDeposit = invalidFills.filter((x) => x.depositId.eq(deposit.depositId));
+    const invalidFillsForDeposit = invalidFills.filter((x) => {
+      const txnUid = `${x.transactionHash}:${x.logIndex}`;
+      // if txnUid doesn't exist in the invalidFills set, add it now, but log the corresponding fill.
+      const newInvalidFill = x.depositId.eq(deposit.depositId) && !this.invalidFills.has(txnUid);
+      if (newInvalidFill) {
+        this.invalidFills.add(txnUid);
+      }
+      return newInvalidFill;
+    });
     if (invalidFillsForDeposit.length > 0) {
       this.logger.warn({
         at: "SpokePoolClient",
