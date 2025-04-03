8c5851e920e88d9286e3b9ca14576710c29e9cad
Paul <108695806+pxrl@users.noreply.github.com>
2025-04-03 14:00:26 +0000

    Diff minimisation

--- src/utils/SpokeUtils.ts
+++ src/utils/SpokeUtils.ts
@@ -5,15 +5,6 @@ import { keccak256 } from "./common";
 import { BigNumber } from "./BigNumberUtils";
 import { isMessageEmpty } from "./DepositUtils";
 
-// Determines if the input address (either a bytes32 or bytes20) is the zero address.
-export function isZeroAddress(address: string): boolean {
-  return address === ZERO_ADDRESS || address === ZERO_BYTES;
-}
-
-export function getMessageHash(message: string): string {
-  return isMessageEmpty(message) ? ZERO_BYTES : keccak256(message);
-}
-
 export function isUnsafeDepositId(depositId: BigNumber): boolean {
   // SpokePool.unsafeDepositV3() produces a uint256 depositId by hashing the msg.sender, depositor and input
   // uint256 depositNonce. There is a possibility that this resultant uint256 is less than the maxSafeDepositId (i.e.
@@ -61,4 +52,13 @@ export function getRelayDataHash(relayData: RelayData, destinationChainId: numbe
       [_relayData, destinationChainId]
     )
   );
+}
+
+// Determines if the input address (either a bytes32 or bytes20) is the zero address.
+export function isZeroAddress(address: string): boolean {
+  return address === ZERO_ADDRESS || address === ZERO_BYTES;
+}
+
+export function getMessageHash(message: string): string {
+  return isMessageEmpty(message) ? ZERO_BYTES : keccak256(message);
 }
