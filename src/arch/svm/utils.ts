b6eb601c367c4c352eb24947225a4994e28ba72e
Melisa Guevara <melisaguevarav@gmail.com>
2025-04-16 13:42:10 -0300

    fix programId type

--- src/arch/svm/utils.ts
+++ src/arch/svm/utils.ts
@@ -140,11 +140,11 @@ export function unwrapEventData(
  * @param extraSeed An optional extra seed. Defaults to 0.
  * @returns The PDA for the State account.
  */
-export async function getStatePda(programId: string, extraSeed = 0): Promise<Address> {
+export async function getStatePda(programId: Address, extraSeed = 0): Promise<Address> {
   const seedEncoder = getU64Encoder();
   const encodedExtraSeed = seedEncoder.encode(extraSeed);
   const [statePda] = await getProgramDerivedAddress({
-    programAddress: address(programId),
+    programAddress: programId,
     seeds: ["state", encodedExtraSeed],
   });
   return statePda;
