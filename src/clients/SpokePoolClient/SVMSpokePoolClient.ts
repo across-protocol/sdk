bf5dda6ebcf8b7866fa5287be2e01b0880c3650b
james-a-morris <jaamorris@cs.stonybrook.edu>
2025-04-22 10:51:37 -0400

    improve: use max lookback

--- src/clients/SpokePoolClient/SVMSpokePoolClient.ts
+++ src/clients/SpokePoolClient/SVMSpokePoolClient.ts
@@ -111,7 +111,10 @@ export class SvmSpokePoolClient extends SpokePoolClient {
         const events = await this.svmEventsClient.queryEvents(
           eventName as SVMEventNames,
           BigInt(config.fromBlock),
-          BigInt(config.toBlock)
+          BigInt(config.toBlock),
+          {
+            limit: config.maxBlockLookBack,
+          }
         );
         return Promise.all(
           events.map(async (event): Promise<SortableEvent> => {
