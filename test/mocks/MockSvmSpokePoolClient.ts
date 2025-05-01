0c8ea55c77e35f538e6a2df17e62e3daa1c7ab6e
Melisa Guevara <melisaguevarav@gmail.com>
2025-05-01 13:08:41 -0300

    WIP: Mock SVMSpokePoolClient

---
+++ test/mocks/MockSvmSpokePoolClient.ts
@@ -0,0 +1,128 @@
+import winston from "winston";
+import { SvmSpokeClient } from "@across-protocol/contracts";
+import { address, Address } from "@solana/kit";
+import {
+  DepositWithBlock,
+  RelayerRefundExecution,
+  SlowFillRequest,
+  SortableEvent,
+  SlowFillLeaf,
+  TokensBridged,
+  Log,
+} from "../../src/interfaces";
+import { getCurrentTime, BigNumber, bnZero, MakeOptional, EventSearchConfig } from "../../src/utils";
+import { SpokePoolUpdate, SvmSpokePoolClient } from "../../src/clients/SpokePoolClient";
+import { HubPoolClient } from "../../src/clients/HubPoolClient";
+import { EventOverrides } from "../../src/clients/mocks/MockEvents";
+import { AcrossConfigStoreClient } from "../../src/clients/AcrossConfigStoreClient";
+import { MockSolanaEventClient } from "./MockSolanaEventClient";
+import { EventWithData, SvmCpiEventsClient, SVMEventNames, unwrapEventData } from "../../src/arch/svm";
+
+// This class replaces internal SpokePoolClient functionality, enabling
+// the user to bypass on-chain queries and inject events directly.
+export class MockSvmSpokePoolClient extends SvmSpokePoolClient {
+  private mockEventsClient: MockSolanaEventClient;
+  private destinationTokenForChainOverride: Record<number, string> = {};
+  public depositIdAtBlock: BigNumber[] = []; // needed?
+
+  constructor(
+    logger: winston.Logger,
+    chainId: number,
+    programId = address("JAZWcGrpSWNPTBj8QtJ9UyQqhJCDhG9GJkDeMf5NQBiq"),
+    deploymentBlock: number = 1,
+    eventSearchConfig: MakeOptional<EventSearchConfig, "to"> = { from: 0, maxLookBack: 0 },
+    opts: { hubPoolClient: HubPoolClient | null } = { hubPoolClient: null }
+  ) {
+    super(
+      logger,
+      opts.hubPoolClient,
+      chainId,
+      BigInt(deploymentBlock),
+      eventSearchConfig,
+      null as unknown as SvmCpiEventsClient,
+      programId,
+      null as unknown as Address
+    );
+    this.mockEventsClient = new MockSolanaEventClient();
+    this.latestHeightSearched = deploymentBlock; // needed?
+  }
+
+  setConfigStoreClient(configStore?: AcrossConfigStoreClient): void {
+    this.configStoreClient = configStore;
+  }
+
+  setDestinationTokenForChain(chainId: number, token: string): void {
+    this.destinationTokenForChainOverride[chainId] = token;
+  }
+
+  getDestinationTokenForDeposit(deposit: DepositWithBlock): string {
+    return this.destinationTokenForChainOverride[deposit.originChainId] ?? super.getDestinationTokenForDeposit(deposit);
+  }
+
+  setLatestBlockNumber(blockNumber: number): void {
+    this.latestHeightSearched = blockNumber;
+  }
+
+  async _update(eventsToQuery: string[]): Promise<SpokePoolUpdate> {
+    const from = this.eventSearchConfig.from ? BigInt(this.eventSearchConfig.from) : undefined;
+    const to = this.eventSearchConfig.to ? BigInt(this.eventSearchConfig.to) : undefined;
+
+    // Get events from the mock event client.
+    const events: EventWithData[][] = await Promise.all(
+      eventsToQuery.map((eventName) => this.mockEventsClient.queryEvents(eventName as SVMEventNames, from, to))
+    );
+
+    const eventsWithBlockNumber = events.map((eventList) =>
+      eventList.map((event) => {
+        return {
+          transactionHash: event.signature,
+          blockNumber: Number(event.slot),
+          transactionIndex: 0,
+          logIndex: 0,
+          ...(unwrapEventData(event.data) as Record<string, unknown>),
+        };
+      })
+    );
+
+    return Promise.resolve({
+      success: true,
+      firstDepositId: bnZero,
+      currentTime: getCurrentTime(),
+      events: eventsWithBlockNumber,
+      searchEndBlock: this.eventSearchConfig.to || this.latestHeightSearched,
+    });
+  }
+
+  deposit(deposit: SvmSpokeClient.FundsDeposited & Partial<EventWithData>): EventWithData {
+    return this.mockEventsClient.deposit("FundsDeposited", deposit);
+  }
+
+  fillRelay(fill: SvmSpokeClient.FilledRelay & Partial<EventWithData>): EventWithData {
+    return this.mockEventsClient.fillRelay("FilledRelay", fill);
+  }
+
+  setTokensBridged(tokensBridged: TokensBridged): EventWithData {
+    // TODO: implement this if needed
+  }
+
+  requestSlowFill(request: Omit<SlowFillRequest, "messageHash"> & Partial<SortableEvent>): Log {
+    // TODO: implement this
+  }
+
+  executeV3SlowRelayLeaf(leaf: Omit<SlowFillLeaf, "messageHash">): Log {
+    // TODO: implement this if needed
+  }
+
+  executeRelayerRefundLeaf(refund: RelayerRefundExecution & Partial<SortableEvent>): Log {
+    // TODO: implement this if needed
+  }
+
+  setEnableRoute(
+    originToken: string,
+    destinationChainId: number,
+    enabled: boolean,
+    overrides: EventOverrides = {}
+  ): Log {
+    // TODO: implement this if needed
+  }
+}
