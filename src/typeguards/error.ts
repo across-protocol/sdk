f5ce0c6bccf297b3095fd54c287738f7ff65dd2d
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-06-25 10:57:50 -0400

    feat: Added isViemError checker

--- src/typeguards/error.ts
+++ src/typeguards/error.ts
@@ -1,7 +1,10 @@
 import { ethers } from "ethers";
 import { EthersError } from "../interfaces";
+import { BaseError } from "viem";
 
 export const isError = (error: unknown): error is Error => error instanceof Error;
 
 export const isEthersError = (error?: unknown): error is EthersError =>
   (error as EthersError)?.code in ethers.utils.Logger.errors;
+
+export const isViemError = (error?: unknown): error is BaseError => error instanceof BaseError;
