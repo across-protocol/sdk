fbc580bba7790455670cd533166f02642129d1f4
Aleksa Colovic <aleksa.colovic98@gmail.com>
2025-06-25 17:16:58 +0200

    Lint fix

--- src/clients/BundleDataClient/BundleDataClient.ts
+++ src/clients/BundleDataClient/BundleDataClient.ts
@@ -43,6 +43,7 @@ import {
   isValidEvmAddress,
   duplicateEvent,
   invalidOutputToken,
+  getNetworkName,
 } from "../../utils";
 import winston from "winston";
 import {
@@ -466,7 +467,7 @@ export class BundleDataClient {
         assert(isDefined(matchingDeposit), "Deposit not found for fill.");
 
         const spokeClient = this.spokePoolClientManager.getSpokePoolClientByChainId(_fill.destinationChainId);
-        assert(isDefined(spokeClient), `SpokePoolClient not found for fill.`);
+        assert(isDefined(spokeClient), `SpokePoolClient for ${getNetworkName(_fill.destinationChainId)} not found for fill.`);
 
         let provider;
         if (isEVMSpokePoolClient(spokeClient)) {
