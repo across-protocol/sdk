fd2a7b85c833e7c9a1c84059d11c3069d31c6aa3
nicholaspai <npai.nyc@gmail.com>
2025-06-26 11:41:49 -0400

    feat(BundleDataClient): Handle deposits where originChain == destinationChain

--- src/clients/BundleDataClient/BundleDataClient.ts
+++ src/clients/BundleDataClient/BundleDataClient.ts
@@ -866,9 +866,6 @@ export class BundleDataClient {
       const originChainBlockRange = getBlockRangeForChain(blockRangesForChains, originChainId, chainIds);
 
       for (const destinationChainId of allChainIds) {
-        if (originChainId === destinationChainId) {
-          continue;
-        }
         originClient.getDepositsForDestinationChainWithDuplicates(destinationChainId).forEach((deposit) => {
           if (deposit.blockNumber > originChainBlockRange[1] || isZeroValueDeposit(deposit)) {
             return;
@@ -942,10 +939,6 @@ export class BundleDataClient {
     for (const originChainId of allChainIds) {
       const originClient = spokePoolClients[originChainId];
       for (const destinationChainId of allChainIds) {
-        if (originChainId === destinationChainId) {
-          continue;
-        }
-
         const destinationClient = spokePoolClients[destinationChainId];
         const destinationChainBlockRange = getBlockRangeForChain(blockRangesForChains, destinationChainId, chainIds);
         const originChainBlockRange = getBlockRangeForChain(blockRangesForChains, originChainId, chainIds);
