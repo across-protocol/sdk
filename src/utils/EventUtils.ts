de9825a3dc19d88700623c00284f6beb59dc7ec2
Ihor Farion <ihor@umaproject.org>
2025-07-16 09:10:39 -0700

    remove knownAddressFields logic from event parsing. Fix one related test
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- src/utils/EventUtils.ts
+++ src/utils/EventUtils.ts
@@ -8,22 +8,6 @@ import { isDefined, toBN, BigNumberish } from "./";
 const maxRetries = 3;
 const retrySleepTime = 10;
 
-// Event fields which changed from an `address` to `bytes32` after the SVM contract upgrade.
-const knownExtendedAddressFields = [
-  // TokensBridged
-  "l2TokenAddress",
-  // FundsDeposited/FilledRelay/RequestedSlowFill
-  "inputToken",
-  "outputToken",
-  "depositor",
-  "recipient",
-  "exclusiveRelayer",
-  // FilledRelay
-  "relayer",
-  // RequestedSpeedUpDeposit
-  "updatedRecipient",
-];
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function spreadEvent(args: Result | Record<string, unknown>): { [key: string]: any } {
   const keys = Object.keys(args).filter((key: string) => isNaN(+key)); // Extract non-numeric keys.
@@ -81,13 +65,6 @@ export function spreadEvent(args: Result | Record<string, unknown>): { [key: str
     // Assuming a numeric output, we can safely cast the unknown to BigNumberish since the depositId will either be a uint32 (and therefore a TypeScript `number`),
     // or a uint256 (and therefore an ethers BigNumber).
     returnedObject.depositId = toBN(returnedObject.depositId as BigNumberish);
-  }
-
-  // Truncate all fields which may be bytes32 into a bytes20 string.
-  for (const field of knownExtendedAddressFields) {
-    if (isDefined(returnedObject[field])) {
-      returnedObject[field] = String(returnedObject[field]);
-    }
   }
 
   return returnedObject;
