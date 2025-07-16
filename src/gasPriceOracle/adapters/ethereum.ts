6f82f3380abcf9da2d329e337d201961894e9d1b
bennett <bennett@umaproject.org>
2025-07-16 12:22:29 -0500

    Revert "fix"
    This reverts commit d0887790ed51fd7cd5e62b83e0686cdac8c00bd8.

--- src/gasPriceOracle/adapters/ethereum.ts
+++ src/gasPriceOracle/adapters/ethereum.ts
@@ -63,14 +63,8 @@ export async function legacy(
 
   if (!BigNumber.isBigNumber(gasPrice) || gasPrice.lt(bnZero)) gasPriceError("getGasPrice()", chainId, gasPrice);
 
-  let maxFeePerGas = gasPrice.mul(baseFeeMultiplier).div(fixedPointAdjustment);
-  const minFee = parseUnits(process.env[`MIN_FEE_PER_GAS_${chainId}`] || "0", 9);
-  if (maxFeePerGas.lt(minFee)) {
-    maxFeePerGas = minFee;
-  }
-
   return {
-    maxFeePerGas,
+    maxFeePerGas: gasPrice.mul(baseFeeMultiplier).div(fixedPointAdjustment),
     maxPriorityFeePerGas: bnZero,
   };
 }
