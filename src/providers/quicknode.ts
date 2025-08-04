c3fbcecb0d5429b497a5d1957fdcd330de6da873
Paul <108695806+pxrl@users.noreply.github.com>
2025-08-04 21:53:36 +0200

    fix(providers): Add new exceptions for QuickNode. (#1175)

--- src/providers/quicknode.ts
+++ src/providers/quicknode.ts
@@ -1,5 +1,13 @@
-import { CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
+import { CHAIN_IDs, MAINNET_CHAIN_IDs as _MAINNET_CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
 import { RPCTransport } from "./types";
+
+const SNOWFLAKES = {
+  [CHAIN_IDs.BSC]: "bsc",
+  [CHAIN_IDs.POLYGON]: "matic",
+  [CHAIN_IDs.OPTIMISM]: "optimism",
+};
+const SNOWFLAKE_CHAIN_IDs = Object.keys(SNOWFLAKES).map(Number);
+const MAINNET_CHAIN_IDs = Object.keys(_MAINNET_CHAIN_IDs).map(Number);
 
 export function getURL(chainId: number, apiKey: string, transport: RPCTransport): string {
   const envVar = "RPC_PROVIDER_KEY_QUICKNODE_PREFIX";
@@ -8,19 +16,22 @@ export function getURL(chainId: number, apiKey: string, transport: RPCTransport)
     throw new Error(`No API key prefix supplied for QuickNode (${envVar})`);
   }
 
-  /* Ethereum and Optimism are special snowflakes */
+  const domain = "quicknode.pro";
+
+  /* Some chains are special snowflakes */
   if (chainId === CHAIN_IDs.MAINNET) {
-    return `${transport}://${prefix}.quicknode.pro/${apiKey}`;
+    return `${transport}://${prefix}.${domain}/${apiKey}`;
   }
 
-  if (chainId === CHAIN_IDs.OPTIMISM) {
-    return `${transport}://${prefix}.optimism.quicknode.pro/${apiKey}`;
-  }
-
-  const chain = PUBLIC_NETWORKS[chainId]?.name.toLowerCase().replace(" ", "-");
+  let chain = SNOWFLAKES[chainId] ?? PUBLIC_NETWORKS[chainId]?.name;
   if (!chain) {
     throw new Error(`No known QuickNode provider for chainId ${chainId}`);
   }
 
-  return `${transport}://${prefix}.${chain}.quicknode.pro/${apiKey}`;
+  if (MAINNET_CHAIN_IDs.includes(chainId) && !SNOWFLAKE_CHAIN_IDs.includes(chainId)) {
+    chain += "-mainnet";
+  }
+  chain = chain.toLowerCase().replace(" ", "-");
+
+  return `${transport}://${prefix}.${chain}.${domain}/${apiKey}`;
 }
