8defc60ee4a393be34b4212971339b8c0c787c99
Paul <108695806+pxrl@users.noreply.github.com>
2025-08-04 12:29:22 +0200

    improve(providers): Support url generation for QuickNode (#1174)

---
+++ src/providers/quicknode.ts
@@ -0,0 +1,26 @@
+import { CHAIN_IDs, PUBLIC_NETWORKS } from "../constants";
+import { RPCTransport } from "./types";
+
+export function getURL(chainId: number, apiKey: string, transport: RPCTransport): string {
+  const envVar = "RPC_PROVIDER_KEY_QUICKNODE_PREFIX";
+  const prefix = process.env[`${envVar}_${chainId}`] ?? process.env[envVar];
+  if (!prefix) {
+    throw new Error(`No API key prefix supplied for QuickNode (${envVar})`);
+  }
+
+  /* Ethereum and Optimism are special snowflakes */
+  if (chainId === CHAIN_IDs.MAINNET) {
+    return `${transport}://${prefix}.quicknode.pro/${apiKey}`;
+  }
+
+  if (chainId === CHAIN_IDs.OPTIMISM) {
+    return `${transport}://${prefix}.optimism.quicknode.pro/${apiKey}`;
+  }
+
+  const chain = PUBLIC_NETWORKS[chainId]?.name.toLowerCase().replace(" ", "-");
+  if (!chain) {
+    throw new Error(`No known QuickNode provider for chainId ${chainId}`);
+  }
+
+  return `${transport}://${prefix}.${chain}.quicknode.pro/${apiKey}`;
+}
