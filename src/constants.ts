02b15be54255213e60653da0e0bed8f14156da35
Paul <108695806+pxrl@users.noreply.github.com>
2025-08-18 19:10:44 +0000

    improve(utils): Define v2.5 upgrade blocks
    This is required to set the lower bound for some older SpokePool
    fillStatus binary searches.

--- src/constants.ts
+++ src/constants.ts
@@ -93,3 +93,14 @@ export const CUSTOM_GAS_TOKENS = {
   [CHAIN_IDs.LENS]: "GHO",
   [CHAIN_IDs.LENS_SEPOLIA]: "GHO",
 };
+
+// Blocks where SpokePools were upgraded from v2 to v2.5.
+// This is where the fillStatus mapping was introduced.
+export const SPOKEPOOL_UPGRADE_BLOCKS = {
+  [CHAIN_IDs.ARBITRUM]: 183082059,
+  [CHAIN_IDs.BASE]: 10874747,
+  [CHAIN_IDs.MAINNET]: 19277695,
+  [CHAIN_IDs.OPTIMISM]: 116469982,
+  [CHAIN_IDs.POLYGON]: 53793776,
+  [CHAIN_IDs.ZK_SYNC]: 27157340,
+};
