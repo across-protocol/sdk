6b4a822ac246dfe2e771d081692e181e562f5e19
Paul <108695806+pxrl@users.noreply.github.com>
2025-08-19 08:58:22 +0000

    Clarify comment

--- src/constants.ts
+++ src/constants.ts
@@ -94,8 +94,9 @@ export const CUSTOM_GAS_TOKENS = {
   [CHAIN_IDs.LENS_SEPOLIA]: "GHO",
 };
 
-// Blocks where SpokePools were upgraded from v2 to v2.5.
-// This is where the fillStatus mapping was introduced.
+// Blocks where SpokePools were upgraded from v2 to v2.5. This is where the fillStatus mapping
+// was introduced. This mapping should only be updated on subsequent upgrades where there are
+// breaking changes and the exact upgrade block is required to bound certain queries.
 export const SPOKEPOOL_UPGRADE_BLOCKS = {
   [CHAIN_IDs.ARBITRUM]: 183082059,
   [CHAIN_IDs.BASE]: 10874747,
